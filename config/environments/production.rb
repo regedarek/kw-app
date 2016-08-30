a21047a86cb5ed70f32df042f29e68e09720b0ff
Dariusz Finster <darek.finster@gmail.com>
2016-08-30 22:17:43 +0200

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
     :domain => 'gmail.com',
     :user_name => ENV['GMAIL_USER_NAME'],
     :password => ENV['GMAIL_PASSWORD'],
-    :authentication => 'plain',
+    :authentication => 'login',
     :enable_starttls_auto => true 
   }
 
