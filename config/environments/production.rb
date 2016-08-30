b70c56ba60e69bcb96893bc7a1a97d85e5d5eccb
Dariusz Finster <darek.finster@gmail.com>
2016-08-30 21:31:31 +0200

    Update mailer server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address   => "smtp.sendgrid.net",
-      :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-      :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => ENV["SENDGRID_USERNAME"],
-      :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
-      :authentication => 'login',
-      :domain => 'yourdomain.com', # your domain to identify your server when connecting
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => ENV['GMAIL_USER_NAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true 
   }
 
   # Code is not reloaded between requests.
