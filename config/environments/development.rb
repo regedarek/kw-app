2715c30745c1b2928ed6c412115bf19bf91bf2f3
Dariusz Finster <dariusz.finster@xing.com>
2016-09-01 10:51:57 +0200

    Update passwords secrets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'gmail.com',
-    :user_name => ENV['GMAIL_USER_NAME'],
-    :password => ENV['GMAIL_PASSWORD'],
+    :user_name => Rails.application.secrets.gmail_login,
+    :password => Rails.application.secrets.gmail_password,
     :authentication => 'plain',
     :enable_starttls_auto => true 
   }
--- db/schema.rb
+++ db/schema.rb
@@ -16,34 +16,6 @@ ActiveRecord::Schema.define(version: 20160831204756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-
-  create_table "course_applications", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.text   "living_address"
-    t.string "birthday"
-    t.string "birthplace"
-    t.string "pesel"
-    t.text   "main_adress"
-    t.text   "description"
-  end
-
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",                  null: false
