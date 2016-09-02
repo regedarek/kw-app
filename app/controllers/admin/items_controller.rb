8f5f3c86c1fab0fd27d50124551463556dc7c993
Dariusz Finster <darek.finster@gmail.com>
2016-09-02 23:36:54 +0200

    Upadte redirect for items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -29,8 +29,8 @@ module Admin
 
     def destroy
       result = Admin::Items.destroy(params[:id])
-      result.success { redirect_to admin_items_path, notice: 'Usunieto' }
-      result.failure { redirect_to admin_items_path, alert: 'Nie usunieto' }
+      result.success { redirect_to :back, notice: 'Usunieto' }
+      result.failure { redirect_to :back, alert: 'Nie usunieto' }
       result.else_fail!
     end
 
@@ -38,17 +38,17 @@ module Admin
       item = Db::Item.find(params[:id])
       item.update_column(:owner, params[:db_item].fetch(:owner).to_i)
 
-      redirect_to admin_items_path, notice: "Ustawiono wlasciciela przedmiotu na #{::Items::OwnerPresenter.new(item.owner).to_s}"
+      redirect_to :back, notice: "Ustawiono wlasciciela przedmiotu na #{::Items::OwnerPresenter.new(item.owner).to_s}"
     end
 
     def make_rentable
       Db::Item.find(params[:id]).update(rentable: true)
-      redirect_to admin_items_path, notice: 'Udostepniono'
+      redirect_to :back, notice: 'Udostepniono'
     end
 
     def make_urentable
       Db::Item.find(params[:id]).update(rentable: false)
-      redirect_to admin_items_path, notice: 'Zablokowano udostepnianie'
+      redirect_to :back, notice: 'Zablokowano udostepnianie'
     end
 
     private
