a402f1befb55bef1cd76a514221aadeba3e4518d
Dariusz Finster <darek.finster@gmail.com>
2016-10-04 19:29:28 +0200

    Add temp alert

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,10 @@
 class PaymentsController < ApplicationController
   def charge
+    unless current_user.kw_id == 2345
+      return redirect_to :back,
+        alert: 'W tym momencie można płacić tylko gotówką przy odbiorze, skontaktuj się z opiekunem.'
+    end
+
     payment = Db::Payment.find(params[:id])
     result = Payments::CreatePayment.new(payment: payment).create
     result.success { |payment_url:| redirect_to payment_url }
