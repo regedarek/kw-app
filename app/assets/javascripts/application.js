5108bfc4a0f466e629dd0fefdcd4fd4d3a544e0b
lamazi <maciek@emplawyer.pl>
2016-10-09 17:04:32 +0000

    add datepicker to choose date of the route

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
+//= require foundation-datepicker
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
   $(function(){ $(document).foundation(); });
+});
+
+$(document).ready(function(){
+	$('#dp1').fdatepicker({
+		format: 'mm-dd-yyyy',
+		disableDblClickSelection: true
+	});
 });
