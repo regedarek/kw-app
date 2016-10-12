db3ee7ec53c69c15519dfe7151aacfb030a5dd9d
Dariusz Finster <darek.finster@gmail.com>
2016-10-12 22:49:58 +0200

    fix intend

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	$('.js-datepicker').fdatepicker({
-		format: 'mm-dd-yyyy',
-		disableDblClickSelection: true
-	});
+  $('.js-datepicker').fdatepicker({
+    format: 'mm-dd-yyyy',
+    disableDblClickSelection: true
+  });
 });
