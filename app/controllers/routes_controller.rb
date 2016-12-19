1107a8e2792f277cc98944dd5716321ecff5ad4a
Dariusz Finster <dariusz.finster@xing.com>
2016-12-19 13:15:01 +0100

    return

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -17,7 +17,7 @@ class RoutesController < ApplicationController
     @route = Db::Route.new(route_params)
     @valleys = Db::Valley.order(:name)
 
-    redirect_to routes_path, alert: 'Musisz być zalogowany.' unless user_signed_in?
+    return redirect_to routes_path, alert: 'Musisz być zalogowany.' unless user_signed_in?
 
     if @route.save
       redirect_to routes_path, notice: 'Dodano twoje przejście.'
