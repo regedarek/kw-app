259b1f3cafd0b3abc32dbb0f15936e73860fdee0
Dariusz Finster <dariusz.finster@xing.com>
2017-01-25 02:16:01 +0100

    update events

--- app/use_cases/events/form.rb
+++ app/use_cases/events/form.rb
@@ -7,6 +7,9 @@ module Events
                   :price_for_members, :price_for_non_members, :application_date, :payment_date,
                   :account_number, :event_rules_url, :google_group_discussion_url
 
+    validates :name, :place, :event_date, :application_list_url, :google_group_discussion_url, :application_date,
+      presence: true 
+
     def self.model_name
       ActiveModel::Name.new(self, nil, "Event")
     end
