e32990c1464e8287a8d7f072aa25dc9f78afa42b
Dariusz Finster <dariusz.finster@xing.com>
2017-01-25 23:12:43 +0100

    Remove strzlecki page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,3 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-
-  layout :layout_for_page
-
-  private
-
-  def layout_for_page
-    return 'strzelecki' if request.env['SERVER_NAME'].include?('strzelecki')
-
-    case params[:id]
-    when 'strzelecki'
-      'strzelecki'
-    else
-      'application'
-    end
-  end
 end
