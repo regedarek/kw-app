2bbbbab63beec80611b7926c8a4a9d24b3a02828
Dariusz Finster <dariusz.finster@xing.com>
2017-01-25 09:50:01 +0100

    deliver later

--- app/use_cases/strzelecki/edition2017.rb
+++ app/use_cases/strzelecki/edition2017.rb
@@ -4,7 +4,7 @@ module Strzelecki
       if form.valid?
         strzelecki_sign_up = Db::Strzelecki::SignUp.create(form.params)
         Orders::CreateOrder.new(service: strzelecki_sign_up).create
-        StrzeleckiMailer.sign_up(strzelecki_sign_up).deliver
+        StrzeleckiMailer.sign_up(strzelecki_sign_up).deliver_later
         Success.new
       else
         Failure.new(:invalid, form: form)
