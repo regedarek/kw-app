ad633b92686a8ba6c83ee8af51be7bb9ff91e915
Dariusz Finster <dariusz.finster@xing.com>
2017-01-27 16:25:34 +0100

    Add baner to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( strzelecki.css )
 Rails.application.config.assets.precompile += %w( strzelecki-bg.jpg )
+Rails.application.config.assets.precompile += %w( baner.png )
