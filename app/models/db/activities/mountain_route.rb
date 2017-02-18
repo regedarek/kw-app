0a1ec1ddd11c3da7dd0ee9fe51f1afed8b79eae9
Dariusz Finster <darek.finster@gmail.com>
2017-02-18 13:27:10 +0100

    Update partial home

--- app/models/db/activities/mountain_route.rb
+++ app/models/db/activities/mountain_route.rb
@@ -2,6 +2,7 @@ module Db
   module Activities
     class MountainRoute < ActiveRecord::Base
       enum route_type: [:ski, :regular_climbing, :winter_climbing]
+      belongs_to :user
     end
   end
 end
