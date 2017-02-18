211805a89e3fe8fddf94431f7f9f1cb85d072c7e
Dariusz Finster <darek.finster@gmail.com>
2017-02-18 20:07:22 +0100

    Update

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -2,6 +2,14 @@ module Admin
   class ProfilesController < Admin::BaseController
     def index
       @profiles = Db::Profile.all
+
+      respond_to do |format|
+        format.html
+        format.xlsx do
+          disposition = "attachment; filename='strzelecki_#{Time.now.strftime("%Y-%m-%d-%H%M%S")}.xlsx'"
+          response.headers['Content-Disposition'] = disposition
+        end
+      end
     end
   end
 end
