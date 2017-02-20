a9e22f2cb89dc96ac615a06eacc3ba99064eb962
Dariusz Finster <darek.finster@gmail.com>
2017-02-20 12:41:06 +0100

    link database and secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files,  "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
