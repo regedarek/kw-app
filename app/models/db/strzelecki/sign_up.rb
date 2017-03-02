85e7737df5bf90b7219c6ad32148ff5bc3855d8a
Dariusz Finster <dariusz.finster@xing.com>
2017-03-02 09:53:25 +0100

    Update sizes

--- app/models/db/strzelecki/sign_up.rb
+++ app/models/db/strzelecki/sign_up.rb
@@ -5,8 +5,8 @@ module Db
       self.table_name = 'strzelecki_sign_ups'
       enum gender_1: [:male, :female], _suffix: :one
       enum gender_2: [:male, :female], _suffix: :two
-      enum tshirt_size_1: [:s, :m, :l, :xl], _suffix: :one
-      enum tshirt_size_2: [:s, :m, :l, :xl], _suffix: :two
+      enum tshirt_size_1: [:wxs, :ws, :wm, :wl, :wxl, :ms, :mm, :ml, :mxl, :mxxl, :mxxxl], _suffix: :one
+      enum tshirt_size_2: [:wxs, :ws, :wm, :wl, :wxl, :ms, :mm, :ml, :mxl, :mxxl, :mxxxl], _suffix: :two
       enum package_type_1: [:kw, :junior, :standard], _suffix: :one
       enum package_type_2: [:none, :kw, :junior, :standard], _suffix: :two
 
