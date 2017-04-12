9bea49465b687aeedd94e3fedb6b1407fcf240bc
Dariusz Finster <darek.finster@gmail.com>
2017-04-12 15:56:19 +0200

    update

--- app/models/db/membership_fee.rb
+++ app/models/db/membership_fee.rb
@@ -4,5 +4,17 @@ module Db
     has_one :order, through: :service
 
     belongs_to :user, foreign_key: :kw_id, primary_key: :kw_id
+
+    def cost
+      last_year_fee = current_user.membership_fees.find_by(year: Date.today.last_year.year)
+      if last_year_fee.present?
+        if last_year_fee.order.present?
+          if last_year_fee.order.payment.try(:cash) || last_year_fee.order.payment.try(:prepaid?)
+            return 100
+          end
+        end
+      end
+      return 150
+    end
   end
 end
