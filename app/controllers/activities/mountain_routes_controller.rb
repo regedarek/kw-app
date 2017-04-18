ce905e9d6f7e556d0b897c6ae6b5814dcb0cb74f
Dariusz Finster <darek.finster@gmail.com>
2017-04-18 16:28:15 +0200

    quickfix

--- app/controllers/activities/mountain_routes_controller.rb
+++ app/controllers/activities/mountain_routes_controller.rb
@@ -30,9 +30,9 @@ module Activities
     end
 
     def update
-      route = Db::Activities::MountainRoute.find(params[:id])
+      @route = Db::Activities::MountainRoute.find(params[:id])
 
-      if route.update(route_params)
+      if @route.update(route_params)
         redirect_to activities_mountain_route_path(route), notice: t('.updated_successfully')
       else
         render :edit
