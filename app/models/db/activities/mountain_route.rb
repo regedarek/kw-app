e9a977d5f777412d23db0df7dc920facd2c0d7af
Dariusz Finster <darek.finster@gmail.com>
2017-04-18 11:07:17 +0200

    update mountain_routes

--- app/models/db/activities/mountain_route.rb
+++ app/models/db/activities/mountain_route.rb
@@ -2,6 +2,9 @@ module Db
   module Activities
     class MountainRoute < ActiveRecord::Base
       enum route_type: [:ski, :regular_climbing, :winter_climbing]
+
+      validates :name, :rating, :partners, :climbing_date, presence: true
+
       belongs_to :user
     end
   end
