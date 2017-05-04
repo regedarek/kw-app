ad1c75c7b4681fe2611e495d19dcadd5efc3f074
Dariusz Finster <darek.finster@gmail.com>
2017-05-04 20:24:25 +0200

    update

--- app/models/db/profile.rb
+++ app/models/db/profile.rb
@@ -7,6 +7,10 @@ module Db
     ransacker :sections do
       Arel.sql("array_to_string(sections, ',')")
     end
+    ransacker :acomplished_courses do
+      Arel.sql("array_to_string(acomplished_courses, ',')")
+    end
+
     belongs_to :user, foreign_key: :kw_id, primary_key: :kw_id
   end
 end
