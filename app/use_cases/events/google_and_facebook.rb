ef70021437d591541e8d43d21a265d2d9485559c
Dariusz Finster <darek.finster@gmail.com>
2017-05-05 16:00:19 +0200

    update

--- app/use_cases/events/google_and_facebook.rb
+++ app/use_cases/events/google_and_facebook.rb
@@ -8,9 +8,14 @@ module Events
         redirect_url: "urn:ietf:wg:oauth:2.0:oob",
         refresh_token: "1/RtmXGtd-SygIV40mM2z-CQVmCvgzTX7sdaroWcIZkTk"
       )
-      events = cal.find_future_events
-      events[0..5].each do |event|
+      google_events = cal.find_future_events
+      google_events[0..5].each do |event|
         Db::Event.create(name: event.title, description: event.description, event_date: event.start_time, place: event.location)
+      end
+      graph = Koala::Facebook::API.new('EAACEdEose0cBALxj1At0DcteZAJbmHfmY3NnTMrPHxLAtUDsDTbnN28ILRKGVfDOX5t479H9tjXDt9QoThISYzu7nmUPsNZCfIYZCN7JqMpQOz4RUQB3YEcmyEEqarY5hPfCiC5FCdmmTPhy6tdrdRJcalVs3EmjdZAzMHz9mEDFHc4Y2Duv')
+      facebook_events = graph.get_object('KlubWysokogorskiKrakow/events')
+      facebook_events[0..5].each do |event|
+        Db::Event.create(name: event.fetch('name'), event_date: event.fetch('start_time'), place: event.fetch('location'), application_list_url: "https://facebook.com/events/#{event.fetch('id')}")
       end
     end
   end
