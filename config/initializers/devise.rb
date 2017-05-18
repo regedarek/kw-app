2bbbe2f91ff6145ac5a11e5fea1305a625a7d2cb
Dariusz Finster <darek.finster@gmail.com>
2017-05-18 13:10:26 +0200

    update docs for mas

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,7 @@ Devise.setup do |config|
     { access_type: "offline",
     prompt: "consent",
     select_account: true,
-    scope: 'userinfo.email,calendar' }
+    scope: 'userinfo.email' }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
