058696e9b460117b6d83e963ba05f21652dea9e8
Darek Finster <dariusz.finster@gmail.com>
2017-06-22 20:16:16 +0200

    add kw_id to params

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -76,7 +76,7 @@ module Admin
 
     def profile_params
       params.require(:profile).permit(
-        :email, :pesel, :first_name, :last_name, :phone, :profession, :application_date,
+        :kw_id, :email, :pesel, :first_name, :last_name, :phone, :profession, :application_date,
         :birth_date, :birth_place, :city, :postal_code, :main_address, :date_of_death,
         :optional_address, :main_discussion_group, :terms_of_service, :added, :remarks,
         recommended_by: [], acomplished_courses: [], sections: [], position: []
