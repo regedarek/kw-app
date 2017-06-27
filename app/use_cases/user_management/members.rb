46c10225af164aa4d528cb61c8397320f679f182
Darek Finster <dariusz.finster@gmail.com>
2017-06-28 00:29:01 +0200

    Update periodic task

--- app/use_cases/user_management/members.rb
+++ app/use_cases/user_management/members.rb
@@ -2,7 +2,8 @@ module UserManagement
   class Members
     def self.set_regular
       Db::Profile.ransack(position_not_cont: 'regular', application_date_lteq: 1.year.ago).result.find_each do |p|
-        p.position << 'regular'
+        p.position.push('regular').uniq
+        p.position.delete('candidate')
         p.save
       end
     end
