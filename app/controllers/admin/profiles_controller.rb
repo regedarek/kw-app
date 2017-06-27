5199eac108ce10210cc3f7146465a5677f4e93b7
Darek Finster <dariusz.finster@gmail.com>
2017-06-27 22:38:49 +0200

    test

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -5,7 +5,7 @@ module Admin
     def index
       @q = Db::Profile.ransack(params[:q])
       @q.sorts = 'created_at desc' if @q.sorts.empty?
-      @profiles = @q.result.page(params[:page])
+      @profiles = @q.result(distinct: true).page(params[:page])
       @profiles = @profiles.where(accepted: false) unless params[:q]
 
       respond_with do |format|
