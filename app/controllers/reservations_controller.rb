56f785d48c0d1b40ddada82aa898edf2ec43185b
Darek Finster <dariusz.finster@wimdu.com>
2017-12-03 21:46:21 +0100

    Temporary close reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,9 @@
 class ReservationsController < ApplicationController
   def index
-    guarded_date = Guards::Date.new(date: params[:start_date])
-    return redirect_to reservations_path(start_date: guarded_date.nearest_thursday) unless guarded_date.thursday?
-    @available_items = Availability::Items.new(start_date: params[:start_date]).collect
+    redirect_to reservations_path, alert: 'Czasowo wypożyczalnia online została zamknięta'
+   #guarded_date = Guards::Date.new(date: params[:start_date])
+   #return redirect_to reservations_path(start_date: guarded_date.nearest_thursday) unless guarded_date.thursday?
+   #@available_items = Availability::Items.new(start_date: params[:start_date]).collect
   end
 
   def availability
@@ -10,12 +11,14 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    form = Reservations::Form.new(params[:reservations_form])
-    result = Reservations::CreateReservation.create(form: form, user: current_user)
-    result.success { redirect_to reservations_path(start_date: form.start_date.to_s), notice: 'Zarezerwowano przedmiot.' }
-    result.form_invalid { |form:| redirect_to home_path, alert: "Nie dodano z powodu: #{form.errors.messages}" }
-    result.item_already_reserved { |form:| redirect_to reservations_path(start_date: form.start_date.to_s), alert: 'Przedmiot został już zarezerwowany w tym okresie.'}
-    result.else_fail!
+    redirect_to reservations_path, alert: 'Czasowo wypożyczalnia online została zamknięta'
+
+   #form = Reservations::Form.new(params[:reservations_form])
+   #result = Reservations::CreateReservation.create(form: form, user: current_user)
+   #result.success { redirect_to reservations_path(start_date: form.start_date.to_s), notice: 'Zarezerwowano przedmiot.' }
+   #result.form_invalid { |form:| redirect_to home_path, alert: "Nie dodano z powodu: #{form.errors.messages}" }
+   #result.item_already_reserved { |form:| redirect_to reservations_path(start_date: form.start_date.to_s), alert: 'Przedmiot został już zarezerwowany w tym okresie.'}
+   #result.else_fail!
   end
 
   def delete_item
