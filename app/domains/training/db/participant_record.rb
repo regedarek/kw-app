dbeb06453e3d2d2b2a0ba0c0e7d8cf10588091bb
Darek Finster <dariusz.finster@wimdu.com>
2017-12-14 23:06:57 +0100

    Equipment

--- app/domains/training/db/participant_record.rb
+++ app/domains/training/db/participant_record.rb
@@ -1,6 +1,8 @@
 module Training
   module Db
     class ParticipantRecord < ActiveRecord::Base
+      RECOMMENDED_BY = %w(google facebook friends festival poster course)
+      EQUIPMENT = %w(crampons pickaxe)
       self.table_name = 'participants'
 
       belongs_to :course, class_name: 'Training::Db::CourseRecord'
