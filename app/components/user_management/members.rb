810579aa57a78c84f4edc122bd0634371d492b5f
Darek Finster <dariusz.finster@wimdu.com>
2018-01-09 23:52:24 +0100

    hotfix

--- app/components/user_management/members.rb
+++ app/components/user_management/members.rb
@@ -3,7 +3,7 @@ require 'net/http'
 module UserManagement
   class Members
     def self.set_regular
-      Net::HTTP.get(URI('https://hchk.io/83bb1a17-d3ed-4d57-93d5-faa0b8a81021')
+      Net::HTTP.get(URI('https://hchk.io/83bb1a17-d3ed-4d57-93d5-faa0b8a81021'))
 
       Db::Profile.ransack(position_not_cont: 'regular', application_date_lteq: 1.year.ago).result.find_each do |p|
         p.position.push('regular').uniq
