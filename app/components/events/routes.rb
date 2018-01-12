f3c639d761423c103de4f86fc71061f873dc0e43
Darek Finster <dariusz.finster@wimdu.com>
2018-01-12 19:28:58 +0100

    update

--- app/components/events/routes.rb
+++ app/components/events/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'mrozne/zapisy', to: 'events/competitions/sign_ups#index', competition_id: Events::Db::CompetitionRecord.find_by(edition_sym: 'mrozne_2018').try(:id)
-  get 'strzelecki/zapisy', to: 'events/competitions/sign_ups#index', competition_id: Events::Db::CompetitionRecord.find_by(edition_sym: 'strzelecki_2018').try(:id)
+  get 'mrozne/zapisy', to: 'events/competitions/sign_ups#index', competition_id: 4
+  get 'strzelecki/zapisy', to: 'events/competitions/sign_ups#index', competition_id: 3
   scope module: 'events' do
     resources :competitions, only: [] do
       resources :sign_ups, controller: 'competitions/sign_ups'
