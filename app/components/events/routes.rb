ce03412fadc8066c972acb53163fed9dce6ea8e0
Darek Finster <dariusz.finster@wimdu.com>
2018-01-12 19:12:53 +0100

    update

--- app/components/events/routes.rb
+++ app/components/events/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'mrozne/zapisy', to: 'events/competitions/sign_ups#index', competition_id: 2
   scope module: 'events' do
     resources :competitions, only: [] do
       resources :sign_ups, controller: 'competitions/sign_ups'
