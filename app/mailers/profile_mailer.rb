991b3897ac9052ebf2c27bd3dabda413ee513777
Darek Finster <dariusz.finster@wimdu.com>
2018-02-08 14:46:15 +0100

    change feedback email in profile creation

--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -114,8 +114,8 @@ class ProfileMailer < ApplicationMailer
     I18n.with_locale(I18n.locale) do
       mail(
         to: @profile.email,
-        cc: 'kw@kw.krakow.pl',
-        from: 'kw@kw.krakow.pl',
+        cc: 'zgloszenia@kw.krakow.pl',
+        from: 'zgloszenia@kw.krakow.pl',
         subject: "Potwierdzenie zgłoszenia do KW Kraków od #{@profile.first_name} #{@profile.last_name}"
       )
     end
