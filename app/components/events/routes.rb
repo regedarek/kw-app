0b644bee212aba8bd541f23fd2c278b4b4b91fb6
Darek Finster <dariusz.finster@wimdu.com>
2018-02-19 17:02:18 +0100

    add asset

--- app/components/events/routes.rb
+++ app/components/events/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'mrozne/zapisy', to: 'events/competitions/sign_ups#index', competition_id: 4
   get 'strzelecki/zapisy', to: 'events/competitions/sign_ups#index', competition_id: 3
+
   scope module: 'events' do
     resources :competitions, only: [] do
       resources :sign_ups, controller: 'competitions/sign_ups'
