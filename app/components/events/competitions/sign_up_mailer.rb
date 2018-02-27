dcac904c6d1db67da2a766c39a1cdd2e61697c2f
Darek Finster <dariusz.finster@wimdu.com>
2018-02-27 14:45:19 +0100

    change email

--- app/components/events/competitions/sign_up_mailer.rb
+++ app/components/events/competitions/sign_up_mailer.rb
@@ -8,7 +8,7 @@ module Events
 
         mail(
           to: @sign_up.participant_email_1,
-          from: 'mjs@kw.krakow.pl',
+          from: 'strzelecki@kw.krakow.pl',
           subject: "Zapisałeś się na zawody KW Kraków: #{@sign_up.competition_record.name}!"
         )
       end
