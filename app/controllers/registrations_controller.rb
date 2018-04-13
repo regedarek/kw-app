81642808cbf013681d01ff9a54bb6e078764d4e9
Darek Finster <dariusz.finster@wimdu.com>
2018-04-13 15:37:12 +0200

    update profile if someone update user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,5 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     resource.update_without_password(params)
+    profile = Db::Profile.find_by(kw_id: resource_params.fetch(:kw_id))
+    profile.update(resource_params.permit(:kw_id, :first_name, :last_name, :email).to_h) if profile.present?
   end
 end
