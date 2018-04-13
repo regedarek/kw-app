f2e7a741f2522dbe5c993b2fa172bab0335b51ea
Darek Finster <dariusz.finster@wimdu.com>
2018-04-13 12:53:00 +0200

    hotfix

--- app/components/membership/admin/membership_fees_controller.rb
+++ app/components/membership/admin/membership_fees_controller.rb
@@ -5,8 +5,12 @@ module Membership
       append_view_path 'app/components'
 
       def unpaid
-        @profile_unpaid_this_year = Db::Profile.where(kw_id: Membership::FeesRepository.new.get_unpaid_kw_ids_this_year)
-        @profile_unpaid_last_year = Db::Profile.where(kw_id: Membership::FeesRepository.new.get_unpaid_kw_ids_last_year)
+        @profile_unpaid_this_year = Db::Profile
+          .where(kw_id: Membership::FeesRepository.new.get_unpaid_kw_ids_this_year)
+          .where.not('position @> array[?]', 'honorable_kw').where.not('position @> array[?]', 'senior')
+        @profile_unpaid_last_year = Db::Profile
+          .where(kw_id: Membership::FeesRepository.new.get_unpaid_kw_ids_last_year)
+          .where.not('position @> array[?]', 'honorable_kw').where.not('position @> array[?]', 'senior')
       end
     end
   end
