0ff4da9aede70067c2fff26a9daa680c1665a940
Darek Finster <dariusz.finster@wimdu.com>
2018-05-06 00:39:52 +0200

    update

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.time_zone = 'Warsaw'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
