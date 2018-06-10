62dbdd247147aa0fa82498415e3c04a3d6b85309
Darek Finster <dariusz.finster@wimdu.com>
2018-06-10 23:47:00 +0200

    update

--- app/components/training/supplementary/sign_up_record.rb
+++ app/components/training/supplementary/sign_up_record.rb
@@ -3,8 +3,13 @@ module Training
     class SignUpRecord < ActiveRecord::Base
       self.table_name = 'supplementary_sign_ups'
 
+      has_one :payment, as: :payable, dependent: :destroy
       belongs_to :user, class_name: 'Db::User', foreign_key: :user_id
       belongs_to :course, class_name: 'Training::Supplementary::CourseRecord', foreign_key: :course_id
+
+      def description
+        "Zaliczka od #{user.first_name} #{user.last_name} nr legitymacji klubowej: #{user.kw_id} na wydarzenie #{course.title}"
+      end
     end
   end
 end
