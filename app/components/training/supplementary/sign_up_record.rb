84f7a0ff4e00459b28d1753a566452c178065089
Darek Finster <dariusz.finster@wimdu.com>
2018-06-11 13:10:32 +0200

    updapte

--- app/components/training/supplementary/sign_up_record.rb
+++ app/components/training/supplementary/sign_up_record.rb
@@ -3,7 +3,7 @@ module Training
     class SignUpRecord < ActiveRecord::Base
       self.table_name = 'supplementary_sign_ups'
 
-      has_one :payment, as: :payable, dependent: :destroy
+      has_one :payment, as: :payable, dependent: :destroy, class_name: 'Db::Payment'
       belongs_to :user, class_name: 'Db::User', foreign_key: :user_id
       belongs_to :course, class_name: 'Training::Supplementary::CourseRecord', foreign_key: :course_id
 
