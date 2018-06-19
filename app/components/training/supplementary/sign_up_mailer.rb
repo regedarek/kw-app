fb6610b9bb55926e0edfce9cc7d51b142d0991e8
Darek Finster <dariusz.finster@wimdu.com>
2018-06-19 23:31:04 +0200

    update

--- app/components/training/supplementary/sign_up_mailer.rb
+++ app/components/training/supplementary/sign_up_mailer.rb
@@ -9,8 +9,7 @@ module Training
 
         mail(
           to: @sign_up.email,
-          cc: organizer&.email,
-          from: (organizer&.email || 'kw@kw.krakow.pl'),
+          from: organizer.email,
           subject: "Zapisałeś się na wydarzenie KW Kraków: #{@sign_up.course.name}!"
         )
       end
