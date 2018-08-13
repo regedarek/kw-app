0e7a71e02f908a7c3c7ad113ea9bad9f23387c39
Darek Finster <dariusz.finster@wimdu.com>
2018-08-13 16:06:27 +0200

    update

--- app/components/charity/routes.rb
+++ app/components/charity/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   end
 
   get 'darowizny' => 'charity/donations#new'
-  get 'dla-mariusza' => 'charity/donations#new'
+  get 'dla-mariusza' => 'charity/donations#new', as: :mariusz
 end
