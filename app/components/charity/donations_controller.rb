6f37d89fae4907c6508eb480e5304a0aba8b1874
Darek Finster <dariusz.finster@wimdu.com>
2018-08-13 16:28:33 +0200

    update

--- app/components/charity/donations_controller.rb
+++ app/components/charity/donations_controller.rb
@@ -12,7 +12,7 @@ module Charity
 
         result.failure do |errors|
           flash[:error] = errors.values.join(", ")
-          redirect_to new_donation_path
+          redirect_to mariusz_path
         end
       end
     end
