dcfbfd1967c599003694e15677c079a1f9ffc61d
Darek Finster <dariusz.finster@wimdu.com>
2018-08-16 00:42:11 +0200

    update

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -19,7 +19,7 @@ module Admin
     end
 
     def general_meeting
-      @results = Db::Profile.all
+      @results = Db::Profile.all.select{|p| !p.position.include?('candidate') && !p.position.include?('canceled') }
       respond_with do |format|
         format.xlsx do
           disposition = "attachment; filename='walne_zebranie_#{Time.now.strftime("%Y-%m-%d-%H%M%S")}.xlsx'"
