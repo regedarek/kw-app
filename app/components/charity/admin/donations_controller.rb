6107ff67cae598a6deda748684a53112d8135a54
Darek Finster <dariusz.finster@wimdu.com>
2018-08-20 22:57:13 +0200

    update

--- app/components/charity/admin/donations_controller.rb
+++ app/components/charity/admin/donations_controller.rb
@@ -5,6 +5,7 @@ module Charity
       def index
         return redirect_to root_url, alert: 'Nie masz dostępu!' unless user_signed_in? && (current_user.roles.include?('donations') || current_user.admin?)
         @donations = Charity::DonationRecord.includes(:payment).where(payments: { state: 'prepaid' })
+        @donations_sum = Charity::DonationRecord.includes(:payment).where(payments: { state: 'prepaid' }).sum(:cost)
       end
     end
   end
