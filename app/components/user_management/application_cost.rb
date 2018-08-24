dfcc6ff5d7f2d6ddecef8fcaef684edf41d72849
Darek Finster <dariusz.finster@wimdu.com>
2018-08-24 17:04:21 +0200

    update

--- app/components/user_management/application_cost.rb
+++ app/components/user_management/application_cost.rb
@@ -7,6 +7,7 @@ module UserManagement
         return Failure.new(:not_found) if profile.nil?
         year_fee = Date.today.month >= 9 ? 50 : 100
         return UserManagement::Cost.new(year_fee: 0, first_fee: 50) if profile.acomplished_courses.include?('basic_kw')
+        return UserManagement::Cost.new(year_fee: 0, first_fee: 50) if profile.acomplished_courses.include?('cave_kw')
         return UserManagement::Cost.new(year_fee: year_fee, first_fee: 0) if profile.acomplished_courses.include?('instructors')
         return UserManagement::Cost.new(year_fee: year_fee, first_fee: 0) if profile.acomplished_courses.include?('other_club')
 
