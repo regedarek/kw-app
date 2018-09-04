332a05ff260d2d2a5d22469a32f8a5b430791da6
Darek Finster <dariusz.finster@wimdu.com>
2018-09-04 16:42:25 +0200

    update

--- app/components/photo_competition/file_uploader.rb
+++ app/components/photo_competition/file_uploader.rb
@@ -19,5 +19,16 @@ module PhotoCompetition
     def store_dir
       "photo_competitions/#{model.edition.code}/#{model.category.name.parameterize.underscore}"
     end
+
+    def filename
+      "#{secure_token}.#{file.extension}" if original_filename.present?
+    end
+
+    protected
+
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    end
   end
 end
