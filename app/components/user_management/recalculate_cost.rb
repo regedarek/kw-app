afcef6b785409776c5dc79d4dcfca37744dd7113
Darek Finster <dariusz.finster@wimdu.com>
2018-09-11 16:48:35 +0200

    update

--- app/components/user_management/recalculate_cost.rb
+++ app/components/user_management/recalculate_cost.rb
@@ -11,6 +11,8 @@ module UserManagement
           payment_request = Payments::Dotpay::PaymentRequest.new(params: params, type: :fees).execute
           payment_request.success do |payment_url:|
             payment.update(payment_url: payment_url)
+
+            Net::HTTP.get(URI('https://hc-ping.com/d5a75af7-f362-4595-a736-4df5e8169aaf'))
           end
         end
       end
