b85d1b2094f6488cd750f3b928d6bfa28ad4fa09
Darek Finster <dariusz.finster@gmail.com>
2018-10-28 20:08:34 +0100

    update

--- app/controllers/activities/mountain_routes_controller.rb
+++ app/controllers/activities/mountain_routes_controller.rb
@@ -64,10 +64,13 @@ module Activities
 
     def destroy
       route = Db::Activities::MountainRoute.find(params[:id])
-      fail 'not admin' unless user_signed_in? && (current_user.admin? || current_user.id == route.user_id)
-      route.destroy
+      if user_signed_in? && (current_user.admin? || current_user.id == route.user_id)
+        route.destroy
 
-      redirect_to activities_mountain_routes_path, notice: t('.removed_successfully')
+        redirect_to activities_mountain_routes_path, notice: t('.removed_successfully')
+      else
+        redirect_to :back, alert: 'Musisz być właścicielem przejścia lub administratorem'
+      end
     end
 
     private
