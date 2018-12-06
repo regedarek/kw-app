0ed31225d697ca3bc19fe09da2884a0c26f35edb
Darek Finster <dariusz.finster@gmail.com>
2018-12-06 11:16:51 +0100

    update

--- app/controllers/admin/membership_fees_controller.rb
+++ app/controllers/admin/membership_fees_controller.rb
@@ -27,8 +27,8 @@ module Admin
 
     def destroy
       result = Admin::MembershipFees.destroy(params[:id])
-      result.success { redirect_to admin_membership_fees_path, notice: 'Usunieto' }
-      result.failure { redirect_to admin_membership_fees_path, alert: 'Nie usunieto' }
+      result.success { redirect_to :back, notice: 'Usunieto' }
+      result.failure { redirect_to :back, alert: 'Nie usunieto' }
       result.else_fail!
     end
 
