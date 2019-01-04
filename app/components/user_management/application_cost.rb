67445f23b64ab1955047b184f82aa7012951a474
Darek Finster <dariusz.finster@gmail.com>
2019-01-04 13:34:22 +0100

    update application cost

--- app/components/user_management/application_cost.rb
+++ app/components/user_management/application_cost.rb
@@ -5,13 +5,19 @@ module UserManagement
     class << self
       def for(profile:)
         return Failure.new(:not_found) if profile.nil?
-        year_fee = Date.today.month >= 9 ? 50 : 100
+
         return UserManagement::Cost.new(year_fee: 0, first_fee: 50, plastic: profile.plastic) if profile.acomplished_courses.include?('basic_kw')
         return UserManagement::Cost.new(year_fee: 0, first_fee: 50, plastic: profile.plastic) if profile.acomplished_courses.include?('cave_kw')
         return UserManagement::Cost.new(year_fee: year_fee, first_fee: 0, plastic: profile.plastic) if profile.acomplished_courses.include?('instructors')
         return UserManagement::Cost.new(year_fee: year_fee, first_fee: 0, plastic: profile.plastic) if profile.acomplished_courses.include?('other_club')
 
         UserManagement::Cost.new(year_fee: year_fee, first_fee: 50, plastic: profile.plastic)
+      end
+
+      private
+
+      def year_fee
+        Date.current.between?(Date.new(Date.current.year, 9, 1), Date.new(Date.current.year, 11, 15)) ? 50 : 100
       end
     end
   end
--- spec/membership/application_cost_spec.rb
+++ spec/membership/application_cost_spec.rb
@@ -22,7 +22,7 @@ describe UserManagement::ApplicationCost do
       end
     end
 
-    context 'months 9-12' do
+    context 'months 9-11' do
       before { Timecop.freeze('2016-10-19'.to_date) }
       it 'other' do
         profile = Factories::Profile.build_form
@@ -33,6 +33,17 @@ describe UserManagement::ApplicationCost do
       end
     end
 
+    context 'months 11-12' do
+      before { Timecop.freeze('2016-11-19'.to_date) }
+      it 'other' do
+        profile = Factories::Profile.build_form
+        result = UserManagement::ApplicationCost.for(profile: profile)
+
+        expect(result.first_fee).to eq(50)
+        expect(result.year_fee).to eq(100)
+      end
+    end
+
     context 'months 1-8' do
       before { Timecop.freeze('2016-06-19'.to_date) }
       it 'other club' do
@@ -44,7 +55,7 @@ describe UserManagement::ApplicationCost do
       end
     end
 
-    context 'months 9-12' do
+    context 'months 9-11' do
       before { Timecop.freeze('2016-10-19'.to_date) }
       it 'other club' do
         profile = Factories::Profile.build_form(acomplished_courses: ['basic', 'other_club'])
@@ -52,6 +63,17 @@ describe UserManagement::ApplicationCost do
 
         expect(result.first_fee).to eq(0)
         expect(result.year_fee).to eq(50)
+      end
+    end
+
+    context 'months 11-12' do
+      before { Timecop.freeze('2016-11-19'.to_date) }
+      it 'other club' do
+        profile = Factories::Profile.build_form(acomplished_courses: ['basic', 'other_club'])
+        result = UserManagement::ApplicationCost.for(profile: profile)
+
+        expect(result.first_fee).to eq(0)
+        expect(result.year_fee).to eq(100)
       end
     end
 
@@ -66,7 +88,7 @@ describe UserManagement::ApplicationCost do
       end
     end
 
-    context 'months 9-12' do
+    context 'months 9-11' do
       before { Timecop.freeze('2016-10-19'.to_date) }
       it 'reactivation' do
         profile = Factories::Profile.build_form
@@ -74,6 +96,17 @@ describe UserManagement::ApplicationCost do
 
         expect(result.first_fee).to eq(50)
         expect(result.year_fee).to eq(50)
+      end
+    end
+
+    context 'months 11-12' do
+      before { Timecop.freeze('2016-11-19'.to_date) }
+      it 'reactivation' do
+        profile = Factories::Profile.build_form
+        result = UserManagement::ApplicationCost.for(profile: profile)
+
+        expect(result.first_fee).to eq(50)
+        expect(result.year_fee).to eq(100)
       end
     end
 
@@ -88,8 +121,19 @@ describe UserManagement::ApplicationCost do
       end
     end
 
-    context 'months 9-12' do
+    context 'months 9-11' do
       before { Timecop.freeze('2016-10-19'.to_date) }
+      it 'internal basic' do
+        profile = Factories::Profile.build_form(acomplished_courses: ['basic_kw'])
+        result = UserManagement::ApplicationCost.for(profile: profile)
+
+        expect(result.first_fee).to eq(50)
+        expect(result.year_fee).to eq(0)
+      end
+    end
+
+    context 'months 11-12' do
+      before { Timecop.freeze('2016-11-19'.to_date) }
       it 'internal basic' do
         profile = Factories::Profile.build_form(acomplished_courses: ['basic_kw'])
         result = UserManagement::ApplicationCost.for(profile: profile)
@@ -110,7 +154,7 @@ describe UserManagement::ApplicationCost do
       end
     end
 
-    context 'months 9-12' do
+    context 'months 9-11' do
       before { Timecop.freeze('2016-10-19'.to_date) }
       it 'external basic' do
         profile = Factories::Profile.build_form(acomplished_courses: ['basic'])
@@ -118,6 +162,17 @@ describe UserManagement::ApplicationCost do
 
         expect(result.first_fee).to eq(50)
         expect(result.year_fee).to eq(50)
+      end
+    end
+
+    context 'months 11-12' do
+      before { Timecop.freeze('2016-11-19'.to_date) }
+      it 'external basic' do
+        profile = Factories::Profile.build_form(acomplished_courses: ['basic'])
+        result = UserManagement::ApplicationCost.for(profile: profile)
+
+        expect(result.first_fee).to eq(50)
+        expect(result.year_fee).to eq(100)
       end
     end
 
@@ -131,7 +186,7 @@ describe UserManagement::ApplicationCost do
       end
     end
 
-    context 'months 9-12' do
+    context 'months 9-11' do
       before { Timecop.freeze('2016-10-19'.to_date) }
       it 'inctructor' do
         profile = Factories::Profile.build_form(acomplished_courses: ['instructors'])
@@ -139,6 +194,17 @@ describe UserManagement::ApplicationCost do
 
         expect(result.first_fee).to eq(0)
         expect(result.year_fee).to eq(50)
+      end
+    end
+
+    context 'months 11-12' do
+      before { Timecop.freeze('2016-11-19'.to_date) }
+      it 'inctructor' do
+        profile = Factories::Profile.build_form(acomplished_courses: ['instructors'])
+        result = UserManagement::ApplicationCost.for(profile: profile)
+
+        expect(result.first_fee).to eq(0)
+        expect(result.year_fee).to eq(100)
       end
     end
   end
