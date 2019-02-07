602e1caeb028f4af578e8e9367691dbf08dcc241
Darek Finster <dariusz.finster@gmail.com>
2019-02-08 00:12:50 +0100

    update

--- app/components/training/activities/repository.rb
+++ app/components/training/activities/repository.rb
@@ -38,61 +38,38 @@ module Training
       def fetch_prev_month
         range_climbing_date = Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month
         range_created_at = Time.now.prev_month.beginning_of_month..(Time.now.prev_month.end_of_month + 2.days)
-        routes = ::Db::Activities::MountainRoute
-          .where(route_type: 'ski', climbing_date: range_climbing_date, created_at: range_created_at).distinct
-        users_ids = routes.collect(&:colleagues).flatten.uniq.map(&:id)
-        users = ::Db::User.where(id: users_ids, boars: true)
-
-        hash = Hash.new
-        users.each do |user|
-          user_routes = routes.select do |route|
-            route.colleagues.include?(user)
-          end
-          hash[user.display_name] = user_routes.map(&:length).compact.sum
-        end
-        hash.sort_by {|_key, value| value}.reverse
+        ::Db::User
+          .joins(:mountain_routes)
+          .where.not(mountain_routes: { id: nil, length: nil })
+          .where(boars: true, mountain_routes: { route_type: 'ski', climbing_date: range_climbing_date, created_at: range_created_at })
+          .select('users.id, SUM(mountain_routes.length)')
+          .group('users.id')
+          .distinct
+          .order('SUM(mountain_routes.length) DESC')
       end
 
       def fetch_current_month
         range = Time.now.beginning_of_month..Time.now.end_of_month
-        routes = ::Db::Activities::MountainRoute
-          .where(
-            route_type: 'ski',
-            climbing_date: range,
-            created_at: range
-        ).uniq
-        users_ids = routes.collect(&:colleagues).flatten.uniq.map(&:id)
-        users = ::Db::User.where(id: users_ids, boars: true)
-
-        hash = Hash.new
-        users.each do |user|
-          user_routes = routes.select do |route|
-            route.colleagues.include?(user)
-          end
-          hash[user.display_name] = user_routes.map(&:length).compact.sum
-        end
-        hash.sort_by {|_key, value| value}.reverse
+        ::Db::User
+          .joins(:mountain_routes)
+          .where.not(mountain_routes: { id: nil, length: nil })
+          .where(boars: true, mountain_routes: { route_type: 'ski', climbing_date: range, created_at: range })
+          .select('users.id, SUM(mountain_routes.length)')
+          .group('users.id')
+          .distinct
+          .order('SUM(mountain_routes.length) DESC')
       end
 
       def fetch_season
         range = start_date..end_date
-        routes = ::Db::Activities::MountainRoute
-          .where(
-            route_type: 'ski',
-            climbing_date: range,
-            created_at: range
-        ).uniq
-        users_ids = routes.collect(&:colleagues).flatten.uniq.map(&:id)
-        users = ::Db::User.where(id: users_ids, boars: true)
-
-        hash = Hash.new
-        users.each do |user|
-          user_routes = routes.select do |route|
-            route.colleagues.include?(user)
-          end
-          hash[user.display_name] = user_routes.map(&:length).compact.sum
-        end
-        hash.sort_by {|_key, value| value}.reverse
+        ::Db::User
+          .joins(:mountain_routes)
+          .where.not(mountain_routes: { id: nil, length: nil })
+          .where(boars: true, mountain_routes: { route_type: 'ski', climbing_date: range, created_at: range })
+          .select('users.id, SUM(mountain_routes.length)')
+          .group('users.id')
+          .distinct
+          .order('SUM(mountain_routes.length) DESC')
       end
 
       def start_date
