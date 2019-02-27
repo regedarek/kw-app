0b9975ab0c676cb7ce2309911302a7d6cdfd3966
Darek Finster <dariusz.finster@gmail.com>
2019-02-27 17:36:55 +0100

    update

--- app/models/db/item.rb
+++ app/models/db/item.rb
@@ -1,7 +1,7 @@
 class Db::Item < ActiveRecord::Base
   enum owner: [:kw, :snw, :sww, :instructors]
-  has_many :reservations, through: :reservation_items
   has_many :reservation_items
+  has_many :reservations, through: :reservation_items
 
   scope :display_name, -> (name) { where('display_name ~* ?', name) }
   scope :rentable_id, -> (id) { where(rentable_id: id) }
