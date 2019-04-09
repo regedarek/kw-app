b0f1d48a7ddb967838cecf8cd1c8911fb4cf827a
Darek Finster <dariusz.finster@gmail.com>
2019-04-09 23:42:30 +0200

    update

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -7,7 +7,7 @@ module Admin
       @q.sorts = 'created_at desc' if @q.sorts.empty?
       @profiles = @q.result(distinct: true).page(params[:page])
       @profiles = @profiles.where(accepted: false) unless params[:q]
-
+      @latest_profile = Db::Profile.where.not(kw_id: [nil, 3884]).order(:kw_id).last
 
       authorize! :read, Db::Profile
 
