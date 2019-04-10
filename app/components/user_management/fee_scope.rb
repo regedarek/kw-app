939d37542b9d4fcb1685c9f4e34a6463adb45f45
Darek Finster <dariusz.finster@gmail.com>
2019-04-10 15:24:11 +0200

    update

--- app/components/user_management/fee_scope.rb
+++ app/components/user_management/fee_scope.rb
@@ -2,7 +2,9 @@ module UserManagement
   class FeeScope
     class << self
       def for(year: Date.today.year)
-        Db::Membership::Fee.includes(:payment).where(year: year, payments: { state: :prepaid }).count
+        Db::Membership::Fee
+          .includes(:payment)
+          .where(year: year, payments: { state: :prepaid }).count
       end
     end
   end
