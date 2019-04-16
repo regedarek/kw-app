ffaeb14761ad1e8e77550d9a3980356bf902f82c
Darek Finster <dariusz.finster@gmail.com>
2019-04-16 17:28:43 +0200

    update

--- app/components/events/competitions/sign_ups/create_form.rb
+++ app/components/events/competitions/sign_ups/create_form.rb
@@ -16,16 +16,16 @@ module Events
         end
 
         define! do
-          required(:single, Types::Form::Bool).filled(:bool?)
-          required(:participant_name_2, Types::String).maybe(:str?)
-          required(:tshirt_size_2, Types::String).maybe(:str?)
-          required(:participant_email_2, Types::String).maybe(:str?)
-          required(:participant_birth_year_2, Types::Form::Int).maybe
-          required(:competition_package_type_2_id, Types::Form::Int).maybe
-          required(:participant_kw_id_2, Types::Form::Int).maybe(:int?, gt?: 1, lt?: 9000)
-          required(:participant_gender_2, Types::Form::Int).maybe
-          required(:participant_city_2, Types::String)
-          required(:participant_team_2, Types::String)
+          required(:single).filled(:bool?)
+          required(:participant_name_2).maybe(:str?)
+          required(:tshirt_size_2).maybe(:str?)
+          required(:participant_email_2).maybe(:str?)
+          required(:participant_birth_year_2).maybe
+          required(:competition_package_type_2_id).maybe
+          required(:participant_kw_id_2).maybe(:int?, gt?: 1, lt?: 9000)
+          required(:participant_gender_2).maybe
+          required(:participant_city_2)
+          required(:participant_team_2)
           rule(participant_gender_2: [:single, :participant_gender_2]) do |single, participant_gender_2|
             single.false?.then(participant_gender_2.filled?)
           end
@@ -63,19 +63,19 @@ module Events
             end
           end
 
-         required(:participant_name_1, Types::String).filled
-         optional(:tshirt_size_1, Types::String).filled
-         optional(:participant_city_1, Types::String)
-         optional(:team_name, Types::String)
-         optional(:teammate_id, Types::String)
-         optional(:remarks, Types::String)
-         optional(:participant_team_1, Types::String)
-         required(:participant_gender_1, Types::Form::Int).filled
-         required(:participant_email_1, Types::String).filled(:str?, format?: /.@.+[.][a-z]{2,}/i)
-         required(:participant_birth_year_1, Types::Form::Int).filled(:int?, gt?: 1920)
-         required(:competition_package_type_1_id, Types::Form::Int).filled
-         optional(:participant_kw_id_1, Types::Form::Int).maybe(:int?, gt?: 1, lt?: 9000)
-         required(:terms_of_service, Types::Form::Bool).value(:true?)
+         required(:participant_name_1).filled
+         optional(:tshirt_size_1).filled
+         optional(:participant_city_1)
+         optional(:team_name)
+         optional(:teammate_id)
+         optional(:remarks)
+         optional(:participant_team_1)
+         required(:participant_gender_1).filled
+         required(:participant_email_1).filled(:str?, format?: /.@.+[.][a-z]{2,}/i)
+         required(:participant_birth_year_1).filled(:int?, gt?: 1920)
+         required(:competition_package_type_1_id).filled
+         optional(:participant_kw_id_1).maybe(:int?, gt?: 1, lt?: 9000)
+         required(:terms_of_service).value(:true?)
 
          validate(active_kw_id_1: [:participant_kw_id_1, :competition_package_type_1_id]) do |kw_id, package_id|
            if Events::Db::CompetitionPackageTypeRecord.find(package_id).membership?
