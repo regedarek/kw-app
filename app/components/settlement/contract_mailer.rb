206e4bbec93bbf742790312af3c09647a0252429
Darek Finster <dariusz.finster@gmail.com>
2019-04-16 17:50:56 +0200

    update

--- app/components/settlement/contract_mailer.rb
+++ app/components/settlement/contract_mailer.rb
@@ -5,8 +5,7 @@ module Settlement
       @office_kings = Db::User.where(":name = ANY(roles)", name: "office_king")
 
       mail(
-        to: @contract.users.map(&:email) + @office_kings.map(&:email),
-        cc: @contract.users.map(&:email) + @office_kings.map(&:email),
+        to: (@contract.users.map(&:email) + @office_kings.map(&:email)).uniq,
         from: 'kw@kw.krakow.pl',
         subject: "Nowe rozliczenie: #{@contract.title} ##{@contract.id}"
       )
