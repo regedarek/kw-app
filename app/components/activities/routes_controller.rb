f08afe2689651ef21058788c1c3d7b4d0c593f45
Darek Finster <dariusz.finster@gmail.com>
2019-04-19 22:52:14 +0200

    update

--- app/components/activities/routes_controller.rb
+++ app/components/activities/routes_controller.rb
@@ -4,7 +4,7 @@ module Activities
     append_view_path 'app/components'
 
     def index
-      @mountain_routes = MountainRouteRecord.all
+      @mountain_routes = MountainRouteRecord.page(params[:page]).per(20)
     end
   end
 end
