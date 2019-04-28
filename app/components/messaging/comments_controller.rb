aeac1442db2b3db91633af96c9dd5200d4cdc339
Darek Finster <dariusz.finster@gmail.com>
2019-04-28 17:06:44 +0200

    update

--- app/components/messaging/comments_controller.rb
+++ app/components/messaging/comments_controller.rb
@@ -26,16 +26,11 @@ module Messaging
           )
         end
       when 'Settlement::ContractRecord'
-        comment.commentable.users.map(&:id).reject{|id| id == comment.user_id }.each do |id|
-          NotificationCenter::NotificationRecord.create(
-            recipient_id: id,
-            actor_id: comment.user_id,
-            action: 'commented_contract',
-            notifiable_id: comment.commentable_id,
-            notifiable_type: 'Settlement::ContractRecord'
-          )
-        end
-        Db::User.where(":name = ANY(roles)", name: "office_king").map(&:id).reject{|id| id == comment.user_id }.each do |id|
+        office_king_ids = Db::User.where(":name = ANY(roles)", name: "office_king").map(&:id)
+        contract_user_ids = comment.commentable.users.map(&:id)
+        recepient_ids = (office_king_ids + contract_user_ids).uniq.reject{|id| id == comment.user_id }
+
+        recepient_ids.each do |id|
           NotificationCenter::NotificationRecord.create(
             recipient_id: id,
             actor_id: comment.user_id,
