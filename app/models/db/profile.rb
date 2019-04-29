197ac8d0708eb6076a158788162095c06f6c3151
Darek Finster <dariusz.finster@gmail.com>
2019-04-30 00:49:27 +0200

    update

--- app/models/db/profile.rb
+++ app/models/db/profile.rb
@@ -5,6 +5,10 @@ module Db
     SECTIONS = %w(snw sww stj gtw kts)
     ACOMPLISHED_COURSES = %w(basic_kw basic basic_without_second second second_winter cave cave_kw ski list blank instructors other_club)
 
+    def self.model_name
+      ActiveModel::Name.new("Db::Profile", nil, "Profile")
+    end
+
     has_one :payment, as: :payable, dependent: :destroy
     has_many :membership_fees, foreign_key: :kw_id, primary_key: :kw_id, class_name: 'Db::Membership::Fee'
     has_many :comments, as: :commentable, class_name: 'Messaging::CommentRecord'
