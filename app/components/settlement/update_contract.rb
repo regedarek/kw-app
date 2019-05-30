9200ac70876d958335e1f410295fd3b8a62759fd
Darek Finster <dariusz.finster@gmail.com>
2019-05-31 00:38:12 +0200

    update

--- app/components/settlement/update_contract.rb
+++ app/components/settlement/update_contract.rb
@@ -14,9 +14,9 @@ module Settlement
       contract = Settlement::ContractRecord.find(id)
       contract.update(form_outputs.to_h)
       contract.update(
-        period_date: Date.civil(raw_inputs['period_date(1i)'].to_i, raw_inputs['period_date(2i)'].to_i, raw_inputs['period_date(3i)'].to_i),
-        contractor_id: form_outputs[:contractor_name].first.to_i
+        period_date: Date.civil(raw_inputs['period_date(1i)'].to_i, raw_inputs['period_date(2i)'].to_i, raw_inputs['period_date(3i)'].to_i)
       )
+      contract.update(contractor_id: form_outputs[:contractor_name].first.to_i) if form_outputs.to_h.key?(:contractor_name)
 
       office_king_ids = Db::User.where(":name = ANY(roles)", name: "office_king").map(&:id)
       contract_user_ids = contract.users.map(&:id)
