35c66b4327257046d163ee0071fad5743c8e2b47
Darek Finster <dariusz.finster@gmail.com>
2019-06-04 10:32:01 +0200

    update

--- app/components/settlement/contract_mailer.rb
+++ app/components/settlement/contract_mailer.rb
@@ -3,9 +3,10 @@ module Settlement
     def notify(contract)
       @contract = contract
       @office_kings = Db::User.where(":name = ANY(roles)", name: "office_king")
+      @acceptors = Db::User.where(":name = ANY(roles)", name: "financial_management")
 
       mail(
-        to: ([@contract.creator.email] + @contract.users.map(&:email) + @office_kings.map(&:email)).uniq,
+        to: ([@contract.creator.email] + @contract.users.map(&:email) + @acceptors.map(&:email) + @office_kings.map(&:email)).uniq,
         from: 'kw@kw.krakow.pl',
         subject: "Nowe rozliczenie: #{@contract.title} ##{@contract.id}"
       )
