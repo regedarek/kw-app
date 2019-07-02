6bb70c743cf3b9ab78ac41880f81f27446aa4b92
Darek Finster <dariusz.finster@gmail.com>
2019-07-02 21:41:26 +0200

    update

--- app/components/library/admin/areas_controller.rb
+++ app/components/library/admin/areas_controller.rb
@@ -1,11 +1,31 @@
 module Library
   module Admin
-    class AreasController < ActiveRecord::Base
+    class AreasController < ApplicationController
+      include EitherMatcher
+      append_view_path 'app/components'
+
       def index
+        @areas = ::Library::AreaRecord.all
+      end
+
+      def new
+        @area = Library::AreaRecord.new
       end
 
       def create
+        @area = Library::AreaRecord.new(area_params)
 
+        if @area.save
+          redirect_to admin_areas_path, notice: 'Rejon dodany!'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def area_params
+        params.require(:area).permit(:name, :description)
       end
     end
   end
