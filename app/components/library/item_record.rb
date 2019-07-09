cd5cee305d1d0e65d6f14fa88adfdff6483fc189
Darek Finster <dariusz.finster@gmail.com>
2019-07-09 23:14:29 +0200

    update

--- app/components/library/item_record.rb
+++ app/components/library/item_record.rb
@@ -6,5 +6,10 @@ module Library
 
     has_many :item_authors, class_name: '::Library::ItemAuthorsRecord', foreign_key: :item_id
     has_many :authors, class_name: '::Library::AuthorRecord', through: :item_authors, foreign_key: :author_id, dependent: :destroy
+
+    def authors_names=(ids)
+      self.author_ids = ids
+    end
+    attr_reader :authors_names
   end
 end
