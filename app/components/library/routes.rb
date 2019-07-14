b33d249b212eb8d18987f9f5089625f4c352e9c4
Darek Finster <dariusz.finster@gmail.com>
2019-07-14 22:42:10 +0200

    update

--- app/components/library/routes.rb
+++ app/components/library/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
     resources :items
     resources :authors, only: :show
   end
+
+  get 'biblioteka', to: 'library/items#index', as: :biblioteka
+  get 'biblioteka/:id', to: 'library/items#show', as: :ksiazka
+  get 'biblioteka/autorzy/:id', to: 'library/authors#show', as: :autor
 end
