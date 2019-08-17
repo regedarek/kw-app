1ec3218d8e60bbf8f434a4c75992864b46216a56
Dariusz Finster <darek.finster@gmail.com>
2019-08-17 20:19:59 +0200

    update

--- app/components/scrappers/scrappers_controller.rb
+++ app/components/scrappers/scrappers_controller.rb
@@ -3,11 +3,12 @@ module Scrappers
     append_view_path 'app/components'
 
     def index
-      @current_date = params[:date] ? params[:date] : Date.today.to_s
-      @shmu_record = Scrappers::ShmuRecord.where(diagram_time: Date.today.all_day)
+      @current_date = params[:date] ? params[:date].to_date : Date.today
+
+      @shmu_records = Scrappers::ShmuRecord.where(diagram_time: @current_date.all_day)
       @topr_record = Scrappers::ToprRecord.find_by(time: @current_date)
-      @meteoblue_record = Scrappers::MeteoblueRecord.find_by(time: @current_date)
-      @pogodynka_record = Scrappers::WeatherRecord.last
+      @meteoblue_record = Scrappers::MeteoblueRecord.find_by(time: @current_date.to_s)
+      @pogodynka_record = Scrappers::WeatherRecord.where(created_at: @current_date.all_day)&.first
       @routes_records = Db::Activities::MountainRoute.where(climbing_date: @current_date)
     end
   end
