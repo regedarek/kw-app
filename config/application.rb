a5b323246a4b1f9529750a60a50d268cfe6cb117
Darek Finster <dariusz.finster@gmail.com>
2019-08-23 10:01:02 +0200

    update

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module KwApp
   class Application < Rails::Application
