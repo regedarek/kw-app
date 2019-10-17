0ed7706680f14533ffd766531bd602459f6a2bf5
Darek Finster <dariusz.finster@gmail.com>
2019-10-17 12:47:53 +0200

    update

--- app/components/business/api/courses_controller.rb
+++ app/components/business/api/courses_controller.rb
@@ -3,6 +3,7 @@ module Business
     class CoursesController < ApplicationController
       def index
         courses = Business::CourseRecord.order(starts_at: :asc)
+        courses = courses.limit(params[:limit]) if params[:limit]
 
         render json: courses.to_json
       end
