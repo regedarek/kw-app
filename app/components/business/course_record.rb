3b8dd7f0db93b3badc5701057958c9e701fee7ba
Darek Finster <dariusz.finster@gmail.com>
2019-10-17 17:17:14 +0200

    update

--- app/components/business/course_record.rb
+++ app/components/business/course_record.rb
@@ -1,6 +1,7 @@
 module Business
   class CourseRecord < ActiveRecord::Base
     include Workflow
+    include ActionView::Helpers::AssetTagHelper
     extend FriendlyId
 
     has_many :comments, as: :commentable, class_name: 'Messaging::CommentRecord'
@@ -17,9 +18,10 @@ module Business
     self.table_name = 'business_courses'
     enum activity_type: [
       :winter_abc, :winter_tourist_1, :winter_tourist_2,
-      :skitour_1, :skitour_2, :skitour_3, :cave, :climbing_1,
-      :climbing_2, :full_climbing, :summer_tatra, :winter_tatra_1,
-      :winter_tatra_2, :ice_1, :ice_2, :club_climbing
+      :skitour_1, :skitour_2, :skitour_3,
+      :climbing_1, :climbing_2, :full_climbing, :summer_tatra, :club_climbing,
+      :winter_tatra_1, :winter_tatra_2, :ice_1, :ice_2,
+      :cave
     ]
 
     workflow_column :state
@@ -44,8 +46,26 @@ module Business
       end
     end
 
+    def logo
+      case activity_type
+      when :winter_abc, :winter_tourist_1, :winter_tourist_2
+        'https://panel.kw.krakow.pl/assets/kw-7b39344ecee6060042f85c3875d827e54a32ff867bf12eb62de751249dd20d0c.png'
+      when :skitour_1, :skitour_2, :skitour_3
+        'https://panel.kw.krakow.pl/assets/snw-be2731e417162f8a0868bf081d1971362c768773f10840bdf6d74d9154b4fd4b.png'
+      when :climbing_1, :climbing_2, :full_climbing, :summer_tatra, :club_climbing
+        'https://panel.kw.krakow.pl/assets/sww-4cc5e8fb1b78b03696ab49766a499fb36ecfe698641ec3e3a22d3f1f821184ed.png'
+      when :winter_tatra_1, :winter_tatra_2, :ice_1, :ice_2
+        'https://panel.kw.krakow.pl/assets/sww-4cc5e8fb1b78b03696ab49766a499fb36ecfe698641ec3e3a22d3f1f821184ed.png'
+      when :cave
+        'https://panel.kw.krakow.pl/assets/stj-da6e5b49b22498ae143dfaaf7327744543424caef65ce41e1d0c38c5bef1059d.png'
+      else
+        'https://panel.kw.krakow.pl/assets/kw-7b39344ecee6060042f85c3875d827e54a32ff867bf12eb62de751249dd20d0c.png'
+      end
+    end
+
     def as_json(options={})
       super.merge(
+        logo: logo,
         display_name: I18n.t("activerecord.attributes.#{model_name.i18n_key}.activity_types.#{activity_type}"),
         free_seats: max_seats ? max_seats - seats : 0
       )
