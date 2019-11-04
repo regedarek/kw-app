ed51f315d7c6d4a0badc02dcea2fcc2d5c9a516b
Darek Finster <dariusz.finster@gmail.com>
2019-11-04 12:52:27 +0100

    update

--- app/components/blog/authors.rb
+++ app/components/blog/authors.rb
@@ -8,7 +8,7 @@ module Blog
         active_members: active_members.map do |user|
           ::Blog::Author.from_record(user)
         end,
-        rest_members: rest_members.count
+        rest_members: rest_members
       }
     end
 
@@ -26,15 +26,15 @@ module Blog
     private
 
     def authors
-      Db::User.includes(:mountain_routes).where.not(author_number: nil)
+      Db::User.includes(:mountain_routes).where.not(author_number: nil).order('profiles.application_date')
     end
 
     def active_members
-      Db::User.includes(:mountain_routes).where(author_number: nil, snw_blog: true)
+      Db::User.includes(:profile, :mountain_routes).where(author_number: nil, snw_blog: true).order('profiles.application_date')
     end
 
     def rest_members
-      Db::User.includes(:profile, :mountain_routes).where(author_number: nil, snw_blog: false).active.where('sections @> array[?]', 'snw')
+      Db::User.includes(:profile, :mountain_routes).where(author_number: nil, snw_blog: false).active.where('sections @> array[?]', 'snw').order('profiles.application_date')
     end
   end
 end
