61a645aa656c709c2a4d5fcb5795fa54bd875129
Darek Finster <dariusz.finster@gmail.com>
2019-11-11 06:19:37 +0100

    update

--- app/components/scrappers/topr_degree_uploader.rb
+++ app/components/scrappers/topr_degree_uploader.rb
@@ -8,12 +8,30 @@ module Scrappers
       storage :file
     end
 
+    version :normal do
+      process :efficient_conversion => [200, 68]
+    end
+
     def filename
-      "#{model.created_at.hour}_00.#{file.extension}" if original_filename.present?
+      "#{model.created_at.hour}_00.jpg" if original_filename.present?
     end
 
     def store_dir
       "scrappers/topr/#{model.time}"
+    end
+
+    private
+
+    def efficient_conversion(width, height)
+      manipulate! do |img|
+        img.format("jpg").combine_options do |c|
+          c.fuzz        "3%"
+          c.trim
+          c.resize      "#{width}x#{height}>"
+          c.resize      "#{width}x#{height}<"
+        end
+        img
+      end
     end
   end
 end
