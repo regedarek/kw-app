949ef4379223f4c5528bc657e9476b86c11c1d68
Darek Finster <dariusz.finster@gmail.com>
2019-11-11 07:20:21 +0100

    update

--- app/components/activities/repository.rb
+++ app/components/activities/repository.rb
@@ -1,77 +1,77 @@
 module Activities
   class Repository
-    def fetch_prev_month
+    def fetch_prev_month(route_type = 'regular_climbing')
       range_climbing_date = Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month
       range_created_at = Time.now.prev_month.beginning_of_month..(Time.now.prev_month.end_of_month + 14.days)
       ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range_climbing_date, created_at: range_created_at })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range_climbing_date, created_at: range_created_at })
         .select('users.kw_id, users.id, users.avatar, SUM(mountain_routes.length) AS total_mountain_routes_length')
         .group(:id)
         .order('total_mountain_routes_length DESC')
     end
 
-    def fetch_current_month
+    def fetch_current_month(route_type = 'regular_climbing')
       range = Time.now.beginning_of_month..Time.now.end_of_month
       ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range, created_at: range })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range, created_at: range })
         .select('users.kw_id, users.id, users.avatar, SUM(mountain_routes.length) AS total_mountain_routes_length')
         .group(:id)
         .order('total_mountain_routes_length DESC')
     end
 
-    def fetch_season
+    def fetch_season(route_type = 'regular_climbing')
       range = start_date..end_date
       ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range, created_at: range })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range, created_at: range })
         .select('users.kw_id, users.id, users.avatar, SUM(mountain_routes.length) AS total_mountain_routes_length')
         .group(:id)
         .order('total_mountain_routes_length DESC')
     end
 
-    def best_route_of_season
+    def best_route_of_season(route_type = 'regular_climbing')
       range = start_date..end_date
       us = ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range, created_at: range })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range, created_at: range })
         .select('users.kw_id, users.id, users.avatar, MAX(mountain_routes.hearts_count) AS max_mountain_routes_hearts_count')
         .group(:id)
         .order('max_mountain_routes_hearts_count DESC')
     end
 
-    def best_of_season
+    def best_of_season(route_type = 'regular_climbing')
       range = start_date..end_date
       us = ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range, created_at: range })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range, created_at: range })
         .select('users.kw_id, users.id, users.avatar, SUM(mountain_routes.hearts_count) AS total_mountain_routes_hearts_count')
         .group(:id)
         .order('total_mountain_routes_hearts_count DESC')
     end
 
-    def tatra_uniqe
+    def tatra_uniqe(route_type = 'regular_climbing')
       range = start_date..end_date
       ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range, created_at: range })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range, created_at: range })
         .where("mountain_routes.description LIKE ?", "%#exploratortatr%").uniq
         .sort_by { |u| u.mountain_routes.where("description LIKE '%#exploratortatr%'").count }.reverse!
     end
 
-    def dziadek_gienek
+    def dziadek_gienek(route_type = 'regular_climbing')
       range = start_date..end_date
       ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
-        .where(climbing_boars: true, mountain_routes: { route_type: 'regular_climbing', climbing_date: range, created_at: range })
+        .where(climbing_boars: true, mountain_routes: { route_type: route_type, climbing_date: range, created_at: range })
         .where("mountain_routes.description LIKE ?", "%#dziadekgienek%").uniq
         .sort_by { |u| u.mountain_routes.where("description LIKE '%#dziadekgienek%'").count }.reverse!
     end
--- app/components/blog/dashboard.rb
+++ app/components/blog/dashboard.rb
@@ -1,7 +1,34 @@
 module Blog
   class Dashboard
     def fetch
-      Db::User.all
+      {
+        latest_degree: Scrappers::ToprRecord.last.topr_degree.url(:normal),
+        top_5_boars: top_5_boars,
+        total_meters: routes.sum(:length),
+        last_route: { name: routes.last.name, date: routes.last.climbing_date },
+        max_meters_person: { name: best_person.display_name, meters: top_5_boars.first }
+      }.to_json
+    end
+
+    private
+
+    def top_5_boars
+      Activities::Repository.new.fetch_season(:ski).map do |user|
+        {
+          display_name: Db::User.find_by(kw_id: user.kw_id).display_name,
+          meters: user.total_mountain_routes_length,
+          avatar: user.avatar.url
+        }
+      end
+    end
+
+    def routes
+      Db::Activities::MountainRoute.where(route_type: 'ski').order(:climbing_date)
+    end
+
+    def best_person
+      kw_id = Activities::Repository.new.fetch_season(:ski).first.kw_id
+      Db::User.find_by(kw_id: kw_id)
     end
   end
 end
