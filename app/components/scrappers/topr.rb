4058fe2dc6ce1c14574f4c9f770fd0eb43af45be
Darek Finster <dariusz.finster@gmail.com>
2019-11-11 04:16:04 +0100

    update

--- app/components/scrappers/topr.rb
+++ app/components/scrappers/topr.rb
@@ -4,9 +4,29 @@ module Scrappers
       page = Nokogiri::HTML(open('http://lawiny.topr.pl'))
       statement = page.xpath('/html/body/div[1]/div[2]/div[1]/section[2]/div[1]/div[1]/div/div[2]').text.squish
 
-      Scrappers::ToprRecord.create(
+      record = Scrappers::ToprRecord.create(
         time: Time.now,
         remote_topr_pdf_url: 'http://lawiny.topr.pl/viewpdf'
+      )
+      reader = PDF::Reader.new(record.topr_pdf.path)
+
+      paragraph = ""
+      paragraphs = []
+      reader.pages.each do |page|
+        lines = page.text.scan(/^.+/)
+        lines.each do |line|
+          if line.length > 55
+            paragraph += " #{line}"
+          else
+            paragraph += " #{line}"
+            paragraphs << paragraph
+            paragraph = ""
+          end
+        end
+      end
+
+      record.update(
+        avalanche_degree: paragraphs[4].gsub!(/.*?(?=PROGNOZA)/im, "").split[1]
       )
     end
   end
