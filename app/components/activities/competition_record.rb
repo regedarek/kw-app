d3bbeace12d5bb441a0991a46933ddd4dcaa2439
Darek Finster <dariusz.finster@gmail.com>
2019-11-14 03:17:33 +0100

    update

--- app/components/activities/competition_record.rb
+++ app/components/activities/competition_record.rb
@@ -6,7 +6,7 @@ module Activities
     self.table_name = 'activities_competitions'
 
     enum country: [
-      :poland, :slovakia, :italy, :france, :austria, :czech, :deutchland, :switzerland
+      :poland, :slovakia, :italy, :france, :austria, :czech, :deutchland, :switzerland, :andorra
     ]
 
     friendly_id :slug_candidates, use: :slugged
