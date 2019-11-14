f64e626af444e675f4f5f2004ea23a0c8a306145
Darek Finster <dariusz.finster@gmail.com>
2019-11-14 01:30:53 +0100

    update

--- app/components/activities/competitions_table.rb
+++ app/components/activities/competitions_table.rb
@@ -1,7 +1,11 @@
 module Activities
   class CompetitionsTable
+    include ActionView::Helpers::AssetTagHelper
+    include Rails.application.routes.url_helpers
+    ApplicationController.append_view_path Rails.root.join('app', 'components', 'activities')
+
     def call
-      "<table><b>test</b></table>"
+      ApplicationController.render(partial: "api/competitions/table", locals: { competitions: Activities::CompetitionRecord.all })
     end
   end
 end
