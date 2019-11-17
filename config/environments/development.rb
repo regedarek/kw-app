a882cab8aa912367bce0b076c78ef6b6031badb1
Darek Finster <dariusz.finster@gmail.com>
2019-11-17 15:26:22 +0100

    rails update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -66,12 +67,12 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-  end
+ #config.after_initialize do
+ #  Bullet.enable = true
+ #  Bullet.alert = true
+ #  Bullet.bullet_logger = true
+ #  Bullet.console = true
+ #  Bullet.rails_logger = true
+ #end
 end
 Rails.application.routes.default_url_options[:host] = 'http://localhost:3002'
