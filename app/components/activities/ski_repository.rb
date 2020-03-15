98e1b3e9223d723fcbaf44164cc7a46ab6a6d97b
Darek Finster <dariusz.finster@gmail.com>
2020-03-16 00:39:18 +0100

    update

--- app/components/activities/ski_repository.rb
+++ app/components/activities/ski_repository.rb
@@ -38,6 +38,30 @@ module Activities
         .order('total_mountain_routes_length DESC')
     end
 
+    def fetch_winter
+      range_climbing_date = start_date..DateTime.new(2020, 2, 29)
+      range_created_at = Time.now.prev_month.beginning_of_month..(Time.now.prev_month.end_of_month + 3.days)
+      ::Db::User
+        .joins(:mountain_routes)
+        .where.not(mountain_routes: { id: nil, length: nil })
+        .where(boars: true, mountain_routes: { training: false, route_type: route_type, climbing_date: range_climbing_date, created_at: range_created_at })
+        .select('users.kw_id, users.id, users.avatar, SUM(mountain_routes.length) AS total_mountain_routes_length')
+        .group(:id)
+        .order('total_mountain_routes_length DESC')
+    end
+
+    def fetch_spring
+      range_climbing_date = DateTime.new(2020, 3, 1)..end_date
+      range_created_at = Time.now.prev_month.beginning_of_month..(Time.now.prev_month.end_of_month + 3.days)
+      ::Db::User
+        .joins(:mountain_routes)
+        .where.not(mountain_routes: { id: nil, length: nil })
+        .where(boars: true, mountain_routes: { training: false, route_type: route_type, climbing_date: range_climbing_date, created_at: range_created_at })
+        .select('users.kw_id, users.id, users.avatar, SUM(mountain_routes.length) AS total_mountain_routes_length')
+        .group(:id)
+        .order('total_mountain_routes_length DESC')
+    end
+
     def best_route_of_season
       range = start_date..end_date
       us = ::Db::User
