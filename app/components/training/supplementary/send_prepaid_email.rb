2de2b1a5b4a0cbdf12c917b8e7181cc8eb9cf8c4
Darek Finster <dariusz.finster@gmail.com>
2020-04-11 20:47:50 +0200

    update

--- app/components/training/supplementary/send_prepaid_email.rb
+++ app/components/training/supplementary/send_prepaid_email.rb
@@ -4,7 +4,7 @@ module Training
       def send_prepaid_emails
         prepaid_emails.each do |sign_up|
           Training::Supplementary::SignUpMailer.prepaid_email(sign_up.id).deliver_later
-          sign_up.update(paid_email_sent_at: Time.now)
+          sign_up.update(paid_email_sent_at: Time.zone.now)
         end
       end
 
