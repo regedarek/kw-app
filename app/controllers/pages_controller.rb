39905b13bca6ea19d4cd9f52434e925dfaad3d9a
Darek Finster <dariusz.finster@gmail.com>
2020-06-24 00:27:58 +0200

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   append_view_path 'app/components'
   include HighVoltage::StaticPage
+
+  before_action :authorize
+
+  def authorize
+    if params[:id] == 'wydolnosc'
+      if user_signed_in? && Training::Supplementary::SignUpRecord.where(course_id: 213).map(&:user_id).compact.include?(current_user.id)
+        puts 'git'
+      else
+        redirect_to root_path, alert: 'Nie jesteś uczestnikiem KSKS!'
+      end
+    end
+  end
 end
