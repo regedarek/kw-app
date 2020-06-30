934939cbb33301858f370fe2927fc1e60db04850
Darek Finster <dariusz.finster@gmail.com>
2020-06-30 22:22:31 +0200

    update

--- app/components/activities/repository.rb
+++ app/components/activities/repository.rb
@@ -2,7 +2,7 @@ module Activities
   class Repository
     def fetch_prev_month(route_type = 'regular_climbing')
       range_climbing_date = Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month
-      range_created_at = Time.now.prev_month.beginning_of_month..(Time.now.prev_month.end_of_month + 2.days)
+      range_created_at = Time.now.prev_month.beginning_of_month..(Time.now.prev_month.end_of_month + 10.days)
       ::Db::User
         .joins(:mountain_routes)
         .where.not(mountain_routes: { id: nil, length: nil })
@@ -23,7 +23,7 @@ module Activities
         .order('total_mountain_routes_length DESC')
     end
 
-    def fetch_season(route_type = 'regular_climbing', start_date=DateTime.new(2020, 07, 1), end_date=DateTime.new(2020, 11, 30))
+    def fetch_season(route_type = 'regular_climbing', start_date=DateTime.new(2020, 06, 1), end_date=DateTime.new(2020, 11, 30))
       range = start_date..end_date
       ::Db::User
         .joins(:mountain_routes)
@@ -77,11 +77,11 @@ module Activities
     end
 
     def start_date
-      start_date = DateTime.new(2020, 07, 1)
+      start_date = DateTime.new(2020, 06, 1)
     end
 
     def end_date
-      end_date = DateTime.new(2019, 11, 30)
+      end_date = DateTime.new(2020, 11, 30)
     end
   end
 end
