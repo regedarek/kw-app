26b62e6e5212b1a36cb067361f2e59fcef30f0b1
Darek Finster <dariusz.finster@gmail.com>
2020-07-15 13:17:07 +0200

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,5 +12,13 @@ class PagesController < ApplicationController
         redirect_to root_path, alert: 'Nie jesteś uczestnikiem KSKS!'
       end
     end
+
+    if params[:id] == 'motoryka'
+      if user_signed_in? && Training::Supplementary::SignUpRecord.where(course_id: [214, 215]).map(&:user_id).compact.include?(current_user.id)
+        puts 'git'
+      else
+        redirect_to root_path, alert: 'Nie jesteś uczestnikiem KSKS!'
+      end
+    end
   end
 end
