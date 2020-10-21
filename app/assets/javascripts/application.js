50fc400823bfcb56c10930f5b5cd23c3ad9e8507
Darek Finster <dariusz.finster@gmail.com>
2020-10-21 18:45:36 +0200

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,7 @@ var x = setInterval(function() {
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) + days * 24 - 2);
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) + days * 24);
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
