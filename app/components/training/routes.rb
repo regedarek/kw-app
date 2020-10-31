152ce233b410c2a0aa08fbcd483c801c84b43341
Darek Finster <dariusz.finster@gmail.com>
2020-10-31 21:04:40 +0100

    udpate

--- app/components/training/routes.rb
+++ app/components/training/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   get 'przejscia/wspinaczka' => 'activities/mountain_routes#new', as: :wspinaczka
   get 'wydarzenia/narciarskie' => 'training/supplementary/courses#index', defaults: { category: 'snw' }, as: :ski_events
   get 'wydarzenia/wspinaczkowe' => 'training/supplementary/courses#index', defaults: { category: 'sww' }, as: :climbing_events
-  get 'wydarzenia/webinary' => 'training/supplementary/courses#index', defaults: { category: 'web' }, as: :climbing_events
+  get 'wydarzenia/webinary' => 'training/supplementary/courses#index', defaults: { category: 'web' }, as: :web_events
   get 'wydarzenia' => 'training/supplementary/courses#index', as: :wydarzenia
   get 'wydarzenia/*id' => 'training/supplementary/courses#show', as: :polish_event
   get 'wydarzenia/*slug' => 'training/supplementary/courses#show', as: :polish_event_slug
