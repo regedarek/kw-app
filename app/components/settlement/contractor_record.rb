995d1e9714f922cc305596ceaf771b782925e271
Darek Finster <dariusz.finster@gmail.com>
2020-11-06 17:51:10 +0100

    update

--- app/components/settlement/contractor_record.rb
+++ app/components/settlement/contractor_record.rb
@@ -3,6 +3,9 @@ module Settlement
     self.table_name = 'contractors'
 
     has_many :contracts, class_name: 'Settlement::ContractRecord', foreign_key: :contractor_id
+    has_many :sponsorship_requests, class_name: 'Marketing::SponsorshipRequestRecord', foreign_key: :contractor_id
+    has_many :comments, as: :commentable, class_name: 'Messaging::CommentRecord'
+
     enum reason_type: [:both, :contracts, :sponsor]
   end
 end
