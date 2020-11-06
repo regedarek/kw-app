fe89aad51868c05323f3c647b84e8358ae48b42f
Darek Finster <dariusz.finster@gmail.com>
2020-11-06 22:30:45 +0100

    update

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -30,7 +30,7 @@ module Admin
 
       respond_with do |format|
         format.xlsx do
-          disposition = "attachment; filename='walne_zebranie_#{Time.now.strftime("%Y-%m-%d-%H%M%S")}.xlsx'"
+          disposition = "attachment; filename=walne_zebranie_#{Time.now.strftime("%Y-%m-%d-%H%M%S")}.xlsx"
           response.headers['Content-Disposition'] = disposition
         end
       end
