7980b60caa2c6b15a5accad6c2a58d7420da6bfd
Darek Finster <dariusz.finster@gmail.com>
2020-12-01 20:45:25 +0100

    update

--- app/components/blog/dashboard.rb
+++ app/components/blog/dashboard.rb
@@ -3,7 +3,6 @@ module Blog
     def fetch
       {
         latest_degree: Scrappers::ToprRecord.last.topr_degree.url(:dashboard),
-        top_5_boars: top_5_boars,
         total_meters: routes.sum(:length),
         last_route: { name: routes.last.name, date: routes.last.climbing_date },
         max_meters_person: { name: Db::User.find_by(kw_id: best_person.kw_id).display_name, meters: best_person.total_mountain_routes_length }
@@ -22,22 +21,12 @@ module Blog
       end
     end
 
-    def top_5_boars
-      Activities::SkiRepository.new.fetch_season(:ski, DateTime.new(2020, 12, 1), DateTime.new(2020, 04, 30)).limit(5).map do |user|
-        {
-          display_name: Db::User.find_by(kw_id: user.kw_id).display_name,
-          meters: user.total_mountain_routes_length,
-          avatar: user.avatar.url
-        }
-      end
-    end
-
     def routes
       Db::Activities::MountainRoute.where(route_type: 'ski').order(:climbing_date)
     end
 
     def best_person
-      Activities::SkiRepository.new.fetch_season(:ski, DateTime.new(2019, 11, 1), DateTime.new(2020, 05, 01)).first
+      Activities::SkiRepository.new.fetch_season.first
     end
   end
 end
