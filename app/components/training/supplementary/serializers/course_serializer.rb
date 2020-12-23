d25f2b73c3772e9782dfdfef04b982772b26738f
Darek Finster <dariusz.finster@gmail.com>
2020-12-23 19:18:02 +0100

    update

--- app/components/training/supplementary/serializers/course_serializer.rb
+++ app/components/training/supplementary/serializers/course_serializer.rb
@@ -2,7 +2,7 @@ module Training
   module Supplementary
     module Serializers
       class CourseSerializer < ActiveModel::Serializer
-        attributes :id, :name, :place, :start_date, :end_date, :application_date, :slug, :kind, :limit, :end_application_date
+        attributes :id, :name, :place, :start_date, :end_date, :application_date, :slug, :kind, :limit, :end_application_date, :category
       end
     end
   end
