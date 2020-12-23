170ac4e5bc4bfd6d56cc802651aac1b7c83fbd5e
Darek Finster <dariusz.finster@gmail.com>
2020-12-23 19:17:06 +0100

    update

--- app/components/training/supplementary/api/courses_controller.rb
+++ app/components/training/supplementary/api/courses_controller.rb
@@ -3,7 +3,10 @@ module Training
     module Api
       class CoursesController < ApplicationController
         def index
-          courses = Training::Supplementary::CourseRecord.all
+          courses = Training::Supplementary::CourseRecord.published
+          courses = courses.where(kind: params[:kind]) if params[:kind]
+          courses = courses.where(category: params[:category]) if params[:category]
+          courses = courses.order(params[:order] => :desc) if params[:order]
 
           render json: courses.limit(5), each_serializer: Training::Supplementary::Serializers::CourseSerializer
         end
