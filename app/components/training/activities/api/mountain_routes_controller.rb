b10b94f21fc4cd99a8e6d71906196de34ba21cfb
Darek Finster <dariusz.finster@gmail.com>
2020-12-23 18:48:48 +0100

    update

--- app/components/training/activities/api/mountain_routes_controller.rb
+++ app/components/training/activities/api/mountain_routes_controller.rb
@@ -6,6 +6,7 @@ module Training
           mountain_routes = ::Db::Activities::MountainRoute.where(hidden: false)
           mountain_routes = mountain_routes.where(route_type: params[:route_type]) if params[:route_type]
           mountain_routes = mountain_routes.where(training: params[:training]) if params[:training]
+          mountain_routes = mountain_routes.order(params[:order] => :desc) if params[:order]
 
           render json: mountain_routes.limit(5), each_serializer: Training::Activities::Serializers::MountainRouteSerializer
         end
--- app/components/training/activities/serializers/mountain_route_serializer.rb
+++ app/components/training/activities/serializers/mountain_route_serializer.rb
@@ -2,7 +2,7 @@ module Training
   module Activities
     module Serializers
       class MountainRouteSerializer < ActiveModel::Serializer
-        attributes :id, :name, :persons, :route_type, :climbing_date, :training, :slug, :attachments, :hearts_count, :colleagues_avatars, :partners
+        attributes :id, :name, :persons, :route_type, :climbing_date, :training, :slug, :attachments, :hearts_count, :colleagues_avatars, :partners, :partners?
       end
     end
   end
