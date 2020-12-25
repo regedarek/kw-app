cc3005868204956137e0940331ee5671cc05d631
Darek Finster <dariusz.finster@gmail.com>
2020-12-25 17:50:39 +0100

    update

--- app/components/messaging/comment_record.rb
+++ app/components/messaging/comment_record.rb
@@ -2,6 +2,8 @@ module Messaging
   class CommentRecord < ActiveRecord::Base
     self.table_name = 'comments'
 
+    default_scope { order(created_at: :desc) }
+
     belongs_to :commentable, polymorphic: true
     belongs_to :user, class_name: 'Db::User'
   end
