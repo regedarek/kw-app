f3dd6ad6c367dcb091970fb130f0244f3e36510b
Darek Finster <dariusz.finster@gmail.com>
2020-12-26 00:10:02 +0100

    informations

--- app/components/management/news/informations_controller.rb
+++ app/components/management/news/informations_controller.rb
@@ -13,11 +13,40 @@ module Management
 
       def create
         @information = InformationRecord.new(information_params)
+
+        authorize! :create, @information
+
         if @information.save
           redirect_to '/informacje', notice: 'Dodano informację'
         else
           render :new
         end
+      end
+
+      def show
+        @information = InformationRecord.find(params[:id])
+      end
+
+      def edit
+        @information = InformationRecord.find(params[:id])
+      end
+
+      def update
+        @information = InformationRecord.find(params[:id])
+
+        authorize! :manage, @information
+
+        if @information.update(information_params)
+          redirect_to "/informacje/#{@information.id}", notice: 'Zaktualizowano informację'
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def information_params
+        params.require(:information).permit(:name, :description, :news_type, :group_type, :starred, :web, :url)
       end
     end
   end
