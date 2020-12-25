822e272d3db2debf01f7d4cf3cdecea15b2758f6
Darek Finster <dariusz.finster@gmail.com>
2020-12-25 17:13:35 +0100

    comments mailer

--- app/components/messaging/comments_controller.rb
+++ app/components/messaging/comments_controller.rb
@@ -17,13 +17,14 @@ module Messaging
       case comment.commentable_type
       when 'Db::Activities::MountainRoute'
         comment.commentable.colleague_ids.reject{|id| id == comment.user_id }.each do |id|
-          NotificationCenter::NotificationRecord.create(
+          notification = NotificationCenter::NotificationRecord.create(
             recipient_id: id,
             actor_id: comment.user_id,
             action: 'commented_route',
             notifiable_id: comment.commentable_id,
             notifiable_type: 'Db::Activities::MountainRoute'
           )
+          NotificationCenter::Mailers::CommentsMailer.notify(comment).deliver_later
         end
       when 'Settlement::ContractRecord'
         office_king_ids = Db::User.where(":name = ANY(roles)", name: "office_king").map(&:id)
