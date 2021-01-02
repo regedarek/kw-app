1036364c4748454bb110b10beff5c9794c21325e
Darek Finster <dariusz.finster@gmail.com>
2021-01-02 22:20:38 +0100

    add dependent destroy to cases

--- app/components/management/voting/case_record.rb
+++ app/components/management/voting/case_record.rb
@@ -13,9 +13,9 @@ module Management
       friendly_id :name, use: :slugged
       self.table_name = 'management_cases'
 
-      has_many :comments, as: :commentable, class_name: 'messaging::commentrecord'
-      has_many :votes, class_name: 'Management::Voting::VoteRecord', foreign_key: :case_id
-      has_many :users, through: :votes, foreign_key: :user_id
+      has_many :comments, as: :commentable, class_name: 'Messaging::CommentRecord', dependent: :destroy
+      has_many :votes, class_name: 'Management::Voting::VoteRecord', foreign_key: :case_id, dependent: :destroy
+      has_many :users, through: :votes, foreign_key: :user_id, dependent: :destroy
 
       workflow_column :state
       workflow do
