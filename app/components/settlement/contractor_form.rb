226bb5ef8ce42809a1d232ce602d01bcad60bd2c
Darek Finster <dariusz.finster@gmail.com>
2021-03-18 00:36:30 +0100

    update

--- app/components/settlement/contractor_form.rb
+++ app/components/settlement/contractor_form.rb
@@ -5,6 +5,13 @@ module Settlement
   ContractorForm = Dry::Validation.Params do
     configure { config.messages_file = 'app/components/settlement/errors.yml' }
     configure { config.messages = :i18n }
+    configure do
+      def self.messages
+        super.merge(
+          en: { errors: { unique_nip: 'nip już istnieje' } }
+        )
+      end
+    end
 
     required(:name).filled
     optional(:nip).maybe
@@ -15,5 +22,11 @@ module Settlement
     optional(:contact_name).maybe
     required(:reason_type).filled
     optional(:description).maybe
+
+    validate(unique_nip: :nip) do |nip|
+      true if nip.nil?
+
+      !Settlement::ContractorRecord.exists?(nip: nip)
+    end
   end
 end
