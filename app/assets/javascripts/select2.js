20f4eb4a58ffe17c7822cf5154acf4111503c2ac
Darek Finster <dariusz.finster@gmail.com>
2021-03-19 13:23:05 +0100

    update

--- app/assets/javascripts/select2.js
+++ app/assets/javascripts/select2.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  let selectedTab = window.location.hash;
-  $(selectedTab + "-label").trigger('click');
-
   $('.js-select-multiple').select2({
     theme: "foundation"
   });
