4f3934a465721bf4451df76a7f18922f097eea9f
Darek Finster <dariusz.finster@gmail.com>
2021-03-25 08:11:12 +0100

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Devise::Controllers::Rememberable
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery prepend: true
@@ -28,5 +29,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kw_id, :phone])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :kw_id, :phone])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :kw_id, :phone])
+  end
+
+  def after_sign_in_remember_me(resource)
+    remember_me resource
   end
 end
