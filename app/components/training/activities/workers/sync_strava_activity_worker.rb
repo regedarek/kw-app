7f99fc62eddd954695014f124485c6aaaed476a2
Darek Finster <dariusz.finster@gmail.com>
2021-03-28 14:24:35 +0200

    update

--- app/components/training/activities/workers/sync_strava_activity_worker.rb
+++ app/components/training/activities/workers/sync_strava_activity_worker.rb
@@ -4,16 +4,16 @@ module Training
       class SyncStravaActivityWorker
         include Sidekiq::Worker
 
-        def perform(**params)
-          user = ::Db::User.find(params[:user_id])
-          activity = strava_fetcher.activity(user: user, strava_id: params[:strava_id])
+        def perform(user_id, strava_id, name)
+          user = ::Db::User.find(user_id)
+          activity = strava_fetcher.activity(user: user, strava_id: strava_id)
 
           return false if our_type(activity.type) && user.mountain_routes.exists?(strava_id: activity.id)
           route = user.mountain_routes.create(
             strava_id: activity.id,
             user_id: user.id,
             map_summary_polyline: activity.map.summary_polyline,
-            name: params[:name] || activity.name,
+            name: name || activity.name,
             time: activity.moving_time_in_hours_s,
             distance: activity.distance_in_kilometers,
             route_type: our_type(activity.type),
