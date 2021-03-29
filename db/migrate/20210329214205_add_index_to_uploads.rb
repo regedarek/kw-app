4c9250233f5f727987b1150ecbdf666f93259a74
Darek Finster <dariusz.finster@gmail.com>
2021-03-29 23:48:23 +0200

    update

--- db/migrate/20210329214205_add_index_to_uploads.rb
+++ db/migrate/20210329214205_add_index_to_uploads.rb
@@ -2,5 +2,6 @@ class AddIndexToUploads < ActiveRecord::Migration[5.2]
   def change
     add_index :storage_uploads, [:uploadable_id, :uploadable_type]
     add_index :storage_uploads, :user_id
+    Storage::UploadRecord.where(content_type: nil).each{|u| u.update(content_type: u.file.content_type, file_size: u.file.size) }
   end
 end
