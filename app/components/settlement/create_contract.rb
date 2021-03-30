df3d2004ac731f542216680a52681d8b5367df98
Darek Finster <dariusz.finster@gmail.com>
2021-03-30 14:09:14 +0200

    update

--- app/components/settlement/create_contract.rb
+++ app/components/settlement/create_contract.rb
@@ -13,20 +13,6 @@ module Settlement
 
       contract = repository.create_contract(form_outputs: form_outputs, creator_id: creator_id)
 
-      office_king_ids = Db::User.where(":name = ANY(roles)", name: "office_king").map(&:id)
-      financial_ids = Db::User.where(":name = ANY(roles)", name: "financial_management").map(&:id)
-      contract_user_ids = contract.users.map(&:id)
-      recepient_ids = (office_king_ids + financial_ids + contract_user_ids).compact.uniq.reject{|id| id == creator_id }
-      recepient_ids.each do |recipient_id|
-        NotificationCenter::NotificationRecord.create(
-          recipient_id: recipient_id,
-          actor_id: contract.creator_id,
-          action: 'created_contract',
-          notifiable_id: contract.id,
-          notifiable_type: 'Settlement::ContractRecord'
-        ) if contract && recipient_id && contract&.creator_id
-      end
-
       ::Settlement::ContractMailer.notify(contract).deliver_later
       return Right(contract)
     end
