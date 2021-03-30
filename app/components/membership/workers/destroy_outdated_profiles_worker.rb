2736184f703464aeba9b6c759a086611dc21eff3
Darek Finster <dariusz.finster@gmail.com>
2021-03-30 11:01:46 +0200

    updaet

--- app/components/membership/workers/destroy_outdated_profiles_worker.rb
+++ app/components/membership/workers/destroy_outdated_profiles_worker.rb
@@ -2,11 +2,11 @@ require 'net/http'
 
 module Membership
   module Workers
-    class DestroyOudatedProfilesWorker
+    class DestroyOutdatedProfilesWorker
       include Sidekiq::Worker
 
       def perform(**)
-        outdated_profiles = Membership::FeesRepository.new.find_outdated_not_prepaided_profiles
+        outdated_profiles = ::Membership::FeesRepository.new.find_outdated_not_prepaided_profiles
         outdated_profiles.map(&:destroy)
       end
     end
