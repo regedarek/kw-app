0cbb9357bb40ba7cdd35fd2bd4f04743d15f2aaf
Darek Finster <dariusz.finster@gmail.com>
2021-04-04 10:01:19 +0200

    update

--- app/components/messaging/conversations_controller.rb
+++ app/components/messaging/conversations_controller.rb
@@ -20,17 +20,17 @@ module Messaging
     def new
       authorize! :create, Mailboxer::Conversation
 
-      @recipients = Db::User.where.not(kw_id: nil).not_hidden.active - [current_user]
+      @users = Db::User.where.not(kw_id: nil).not_hidden.active - [current_user]
     end
 
     def create
       authorize! :create, Mailboxer::Conversation
 
-      @recipients = Db::User.where.not(kw_id: nil).not_hidden.active - [current_user]
-      recipient = Db::User.find_by(id: params[:user_id])
+      @users = Db::User.where.not(kw_id: nil).not_hidden.active - [current_user]
+      @recipients = Db::User.where(id: params[:user_ids])
 
-      if recipient && params[:body].present? && params[:subject].present?
-        receipt = current_user.send_message(recipient, params[:body], params[:subject])
+      if @recipients.any? && params[:user_ids] && params[:body].present? && params[:subject].present?
+        receipt = current_user.send_message(@recipients, params[:body], params[:subject])
         redirect_to conversation_path(receipt.conversation)
       else
         flash[:alert] = 'Wypełnij temat i wybierz odbiorcę'
