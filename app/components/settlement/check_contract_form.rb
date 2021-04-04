87faa0ae184136ad2f28f64374147767e47fc9b5
Darek Finster <dariusz.finster@gmail.com>
2021-04-05 00:13:34 +0200

    update

--- app/components/settlement/check_contract_form.rb
+++ app/components/settlement/check_contract_form.rb
@@ -2,7 +2,7 @@ require 'i18n'
 require 'dry-validation'
 
 module Settlement
-  ContractForm = Dry::Validation.Params do
+  CheckContractForm = Dry::Validation.Params do
     configure { config.messages_file = 'app/components/settlement/errors.yml' }
     configure { config.messages = :i18n }
     configure do
