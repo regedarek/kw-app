673f9ea0bdded1d71d3e46cab6fd2467c5980c8c
Darek Finster <dariusz.finster@gmail.com>
2021-04-14 22:38:33 +0200

    update

--- db/migrate/20210414203025_add_passed_date_to_managment_resolutions.rb
+++ db/migrate/20210414203025_add_passed_date_to_managment_resolutions.rb
@@ -1,5 +1,5 @@
 class AddPassedDateToManagmentResolutions < ActiveRecord::Migration[5.2]
   def change
-    add_column :managment_resolutions, :passed_date, :date
+    add_column :management_resolutions, :passed_date, :date
   end
 end
