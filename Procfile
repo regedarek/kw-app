ab88c729dbe70f0e4a3d089e602cf2a3cd922cbb
Darek Finster <dariusz.finster@gmail.com>
2021-04-16 12:42:27 +0200

    update

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: bundle exec sidekiq -e staging -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { password: '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh', url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+  config.redis = { password: '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh', url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { password: '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh', url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+  config.redis = { password: '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh', url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
