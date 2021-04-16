319fafe992824d79a2cdd44b22d20a6794ebaf44
Darek Finster <dariusz.finster@gmail.com>
2021-04-16 13:28:10 +0200

    update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_server do |config|
-  config.redis = { password: ENV.fetch('REDIS_PASSWORD', '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh'), url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
-end
+unless Rails.env.staging?
+  Sidekiq.configure_server do |config|
+    config.redis = { password: ENV.fetch('REDIS_PASSWORD', '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh'), url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { password: ENV.fetch('REDIS_PASSWORD', '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh'), url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+  Sidekiq.configure_client do |config|
+    config.redis = { password: ENV.fetch('REDIS_PASSWORD', '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh'), url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+  end
 end
