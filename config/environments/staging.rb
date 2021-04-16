f6acbd86577439420cb9f8c84d4a5370a55931bd
Darek Finster <dariusz.finster@gmail.com>
2021-04-16 13:34:10 +0200

    update

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.active_job.queue_adapter = :inline
   # Code is not reloaded between requests.
   config.cache_classes = true
 
