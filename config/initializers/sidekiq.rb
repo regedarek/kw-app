8412b87a504c15b0f9bc657b9b3933dbda96d8e7
Darek Finster <dariusz.finster@gmail.com>
2021-04-16 14:21:44 +0200

    update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 if Rails.env.staging?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
-  end
+ #Sidekiq.configure_server do |config|
+ #  config.redis = { url: ENV["REDIS_URL"] }
+ #end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
-  end
+ #Sidekiq.configure_client do |config|
+ #  config.redis = { url: ENV["REDIS_URL"] }
+ #end
 else
   Sidekiq.configure_server do |config|
     config.redis = { password: ENV.fetch('REDIS_PASSWORD', '+eA8tga96sbquDe9CLL3yUZMNdHM6prSwD6kj1vXO4nzPPudDkxh4U+/LMtOWd+Wd72s9MnXNZqCKZeh'), url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
