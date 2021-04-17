4834d14a98009334e16a1e4f9069522b12805cd2
Darek Finster <dariusz.finster@gmail.com>
2021-04-18 01:31:21 +0200

    update

--- app/components/training/supplementary/repository.rb
+++ app/components/training/supplementary/repository.rb
@@ -79,6 +79,7 @@ module Training
         Training::Supplementary::CourseRecord
           .where(start_date: Date.new..Date.current.beginning_of_day)
           .where(category: categories, kind: kinds)
+          .where.not(state: :draft)
           .order(created_at: :desc, start_date: :desc, application_date: :desc).collect do |record|
             Training::Supplementary::Course.from_record(record)
           end
