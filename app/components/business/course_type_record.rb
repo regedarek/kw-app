19292191e14c44c39c4fc3198e786ef3e37f50c1
Darek Finster <dariusz.finster@gmail.com>
2021-10-12 23:38:03 +0200

    update

--- app/components/business/course_type_record.rb
+++ app/components/business/course_type_record.rb
@@ -5,7 +5,7 @@ module Business
     has_many :courses, class_name: '::Business::CourseRecord', foreign_key: :course_type_id
 
     def logo_uri
-      'https://panel.kw.krakow.pl/assets/kw-7b39344ecee6060042f85c3875d827e54a32ff867bf12eb62de751249dd20d0c.png'
+      'kw.png'
     end
   end
 end
