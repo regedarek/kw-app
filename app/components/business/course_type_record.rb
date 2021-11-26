c6e6f61128f3fec0bc8c54f827dda0eaa5355669
Darek Finster <dariusz.finster@gmail.com>
2021-11-26 14:35:45 +0100

    update

--- app/components/business/course_type_record.rb
+++ app/components/business/course_type_record.rb
@@ -5,7 +5,7 @@ module Business
     has_many :courses, class_name: '::Business::CourseRecord', foreign_key: :course_type_id
 
     def logo_uri
-      'kw.png'
+      'https://szkolaalpinizmu.pl/wp-content/uploads/2020/03/kw.png'
     end
   end
 end
