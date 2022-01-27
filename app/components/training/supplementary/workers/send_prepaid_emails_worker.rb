0f9a7deae79ec702e1e6ffd0dddedf5257a6db38
Darek Finster <dariusz.finster@gmail.com>
2022-01-27 18:32:44 +0100

    update

--- app/components/training/supplementary/workers/send_prepaid_emails_worker.rb
+++ app/components/training/supplementary/workers/send_prepaid_emails_worker.rb
@@ -6,6 +6,8 @@ module Training
 
         def perform(sign_up_id)
           sign_up = Training::Supplementary::SignUpRecord.find(sign_up_id)
+          return true if sign_up.course.send_manually?
+
           Training::Supplementary::SignUpMailer.prepaid_email(sign_up.id).deliver_later
           sign_up.update(paid_email_sent_at: Time.zone.now)
         end
