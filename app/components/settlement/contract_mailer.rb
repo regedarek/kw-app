b6a43810a5a20cf9e6030fc7f2fb39b6aa046036
Darek Finster <dariusz.finster@gmail.com>
2022-02-14 16:37:09 +0100

    update

--- app/components/settlement/contract_mailer.rb
+++ app/components/settlement/contract_mailer.rb
@@ -4,11 +4,10 @@ module Settlement
       @contract = contract
       @office_kings = Db::User.where(":name = ANY(roles)", name: "office_king")
       @acceptors = Db::User.where(":name = ANY(roles)", name: "financial_management")
-      @responsibles = Db::User.where(":name = ANY(roles)", name: @contract.group_type)
 
       mail(
-        to: (@responsibles.map(&:email) + [@contract.creator.email] + @contract.users.map(&:email) + @acceptors.map(&:email) + @office_kings.map(&:email)).uniq,
-        from: 'kw@kw.krakow.pl',
+        to: [@contract.creator.email] + @contract.users.map(&:email) + @acceptors.map(&:email) + @office_kings.map(&:email)).uniq,
+        from: 'rozliczenia@kw.krakow.pl',
         subject: "Nowe rozliczenie: #{@contract.title} ##{contract.internal_number}/#{contract.period_date.year}"
       )
     end
@@ -17,11 +16,10 @@ module Settlement
       @contract = contract
       @office_kings = Db::User.where(":name = ANY(roles)", name: "office_king")
       @acceptors = Db::User.where(":name = ANY(roles)", name: "financial_management")
-      @responsibles = Db::User.where(":name = ANY(roles)", name: @contract.group_type)
 
       mail(
-        to: (@responsibles.map(&:email) + [@contract.creator.email] + @contract.users.map(&:email) + @acceptors.map(&:email) + @office_kings.map(&:email)).uniq,
-        from: 'kw@kw.krakow.pl',
+        to: [@contract.creator.email] + @contract.users.map(&:email) + @acceptors.map(&:email) + @office_kings.map(&:email)).uniq,
+        from: 'rozliczenia@kw.krakow.pl',
         subject: "Rozliczenie: #{@contract.title} ##{contract.internal_number}/#{contract.period_date.year} zmieniło status na #{I18n.t(@contract.state, scope: 'activerecord.attributes.settlement/contract_record.states')}"
       )
     end
