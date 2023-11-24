09599459415572ef9cd0883a56df6d2e83aaf5c8
Darek Finster <dariusz.finster@gmail.com>
2023-11-24 18:16:50 +0100

    test

--- app/components/photo_competition/admin/editions_controller.rb
+++ app/components/photo_competition/admin/editions_controller.rb
@@ -9,7 +9,7 @@ module PhotoCompetition
 
       def show
         @edition = PhotoCompetition::EditionRecord.find(params[:id])
-        @photo_requests = PhotoCompetition::RequestRecord.includes(:category, :edition, :user).where(edition_record_id: @edition.id)
+        @photo_requests = PhotoCompetition::RequestRecord.includes(:category, :edition, :user).where(edition_record_id: @edition.id).order(accepted: :asc)
         @q = @photo_requests.order(:created_at).ransack(params[:q])
         @photo_requests = @q.result(distinct: true).page(params[:page])
       end
