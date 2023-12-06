6489149c28b257c6aac185ae9cf839538a8f6d22
Darek Finster <dariusz.finster@gmail.com>
2023-12-06 16:17:42 +0100

    test

--- app/components/photo_competition/admin/editions_controller.rb
+++ app/components/photo_competition/admin/editions_controller.rb
@@ -4,7 +4,7 @@ module PhotoCompetition
       append_view_path 'app/components'
 
       def index
-        @editions = PhotoCompetition::EditionRecord.all
+        @editions = PhotoCompetition::EditionRecord.order(updated_at: :desc)
       end
 
       def show
@@ -16,7 +16,7 @@ module PhotoCompetition
 
         @photo_requests = @photo_requests.where(category_record_id: params[:category_record_id]) if params[:category_record_id].present?
 
-        @q = @photo_requests.order(:created_at).ransack(params[:q])
+        @q = @photo_requests.order(updated_at: :desc).ransack(params[:q])
         @photo_requests = @q.result(distinct: true).page(params[:page]).per(10)
       end
     end
