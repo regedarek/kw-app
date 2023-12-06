26bce30869c8d8858b58fc27bb06af95f312d06b
Darek Finster <dariusz.finster@gmail.com>
2023-12-06 10:34:10 +0100

    test

--- app/components/photo_competition/editions_controller.rb
+++ app/components/photo_competition/editions_controller.rb
@@ -7,11 +7,11 @@ module PhotoCompetition
       if Date.today >= '15-12-2023'.to_date
         @photo_requests = PhotoCompetition::RequestRecord.where(accepted: true, edition_record_id: @edition.id).includes(:category, :edition)
         @photo_requests = @photo_requests.where(category_record_id: params[:category_record_id]) if params[:category_record_id].present?
-        @photo_requests = @photo_requests.order(likes_count: :desc).page(params[:page]).per(8)
+        @photo_requests = @photo_requests.order(likes_count: :desc).page(params[:page]).per(20)
       else
         @photo_requests = PhotoCompetition::RequestRecord.where(accepted: true, edition_record_id: @edition.id).includes(:category, :edition)
         @photo_requests = @photo_requests.where(category_record_id: params[:category_record_id]) if params[:category_record_id].present?
-        @photo_requests = @photo_requests.order(updated_at: :desc).page(params[:page]).per(8)
+        @photo_requests = @photo_requests.order(updated_at: :desc).page(params[:page]).per(20)
       end
     end
   end
