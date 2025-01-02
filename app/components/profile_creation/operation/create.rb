6898f0b228ea80586bc8ee8d3fb2339a553de73f
Darek Finster <dariusz.finster@gmail.com>
2025-01-02 13:01:14 +0100

    profile_creation up

--- app/components/profile_creation/operation/create.rb
+++ app/components/profile_creation/operation/create.rb
@@ -4,7 +4,7 @@ class ProfileCreation::Operation::Create
   def call(params: {})
     profile        = Db::Profile.new
     profile_params = yield validate!(profile: profile, params: params)
-    profile        = yield persist_profile!(profile_params: profile_params)
+    profile        = yield persist_profile!(profile: profile, profile_params: profile_params)
 
     Success(profile)
   end
@@ -15,7 +15,7 @@ class ProfileCreation::Operation::Create
     ProfileCreation::Contract::Create.new
     .call(params)
       .to_monad
-      .fmap { |params| params.to_h }
+      .fmap { |params| params.to_h[:profile] }
       .or do |contract|
         profile = Db::Profile.new(contract.to_h[:profile])
         contract.errors(locale: profile.locale).each do |error|
@@ -26,9 +26,9 @@ class ProfileCreation::Operation::Create
   end
 
   def persist_profile!(profile:, profile_params:)
-    Try do
-      profile.assign_attributes(profile_params)
-      profile.save!
-    end.to_result
+    profile.assign_attributes(profile_params)
+    profile.save
+
+    Success(profile)
   end
 end
