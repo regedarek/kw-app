aaf2d0683ca0cf3aa5bb01d5c44801907d233149
Darek F <dariusz.finster@gmail.com>
2025-03-04 22:01:02 +0100

    skip gtw

--- app/components/business/api/courses_controller.rb
+++ app/components/business/api/courses_controller.rb
@@ -2,7 +2,7 @@ module Business
   module Api
     class CoursesController < ApplicationController
       def index
-        courses = Business::CourseRecord.where(state: 'ready').where("start_date >= ?", Date.today)
+        courses = Business::CourseRecord.where(state: 'ready').where("starts_at >= ?", Date.today)
         courses = courses.order(starts_at: :desc)
         courses = courses.limit(params[:limit]) if params[:limit]
 
