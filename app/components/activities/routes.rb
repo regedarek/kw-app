ec6f29dd0f098e364841b3e65466c0bbe1849e9f
Darek F <dariusz.finster@gmail.com>
2025-03-10 09:58:04 +0100

    skip gtw

--- app/components/activities/routes.rb
+++ app/components/activities/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   get "routes" => 'activities/routes#index'
   get "gorskie-dziki/regulamin" => 'activities/routes#gorskie_dziki_regulamin', as: :gorskie_dziki_regulamin
   get "gorskie-dziki" => 'activities/routes#gorskie_dziki', as: :gorskie_dziki
-  get "tradowe-dziki", to: "activities/routes#liga_tradowa", as: :tradowe_dziki, defaults: { year: -> { Time.current.year } }
+  get "tradowe-dziki", to: "activities/routes#liga_tradowa", as: :tradowe_dziki, defaults: { year: '2025' }
   get "narciarskie-dziki" => 'activities/routes#narciarskie_dziki', as: :narciarskie_dziki
   get "liga-tradowa/:year" => 'activities/routes#liga_tradowa', as: :liga_tradowa
   get "narciarskie-dziki/:year/:month" => 'activities/routes#narciarskie_dziki_month', as: :narciarskie_dziki_month
