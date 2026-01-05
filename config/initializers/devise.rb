9443458a71158c820c5a58590861c8ccad004b7c
Darek Finster <dariusz.finster@gmail.com>
2026-01-05 16:28:37 +0100

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,21 +255,20 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+end
 
-  # ==> Warden configuration
-  config.warden do |manager|
-    manager.after_authentication do |user, auth, opts|
-      # Ensure user is fully loaded with all attributes after authentication
-      if user && user.kw_id.nil?
-        begin
-          user.reload
-        rescue ActiveRecord::RecordNotFound
-          auth.logout
-        end
-      end
+# Warden hooks - must be after Devise.setup
+Warden::Manager.after_set_user do |user, auth, opts|
+  # Ensure user is fully loaded with all attributes after authentication
+  if user && user.kw_id.nil?
+    begin
+      user.reload
+    rescue ActiveRecord::RecordNotFound
+      auth.logout
     end
   end
 end
+
 require 'devise/mailer'
 
 module Devise
