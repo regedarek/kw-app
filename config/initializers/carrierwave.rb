cfef7537292b042a51d660aaf086318d04abfd35
Darek F <dariusz.finster@gmail.com>
2026-01-06 12:11:31 +0100

    Cache avatar URLs and avoid OpenStack calls
    Add migration to store avatar_cached_url and avatar_updated_at

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,10 +8,19 @@ if Rails.env.staging? || Rails.env.production?
       openstack_api_key: Rails.application.secrets.openstack_api_key,
       openstack_username: Rails.application.secrets.openstack_username,
       openstack_auth_url: "https://auth.cloud.ovh.net/",
-      openstack_region: 'WAW'
+      openstack_region: 'WAW',
+      connection_options: {
+        connect_timeout: 5,
+        read_timeout: 5,
+        write_timeout: 5
+      }
     }
     config.asset_host = Rails.application.secrets.openstack_asset_host
     config.fog_directory = "kw-app-cloud-#{Rails.env}"
     config.fog_public = false
+    
+    # Enable attribute caching to avoid repeated OpenStack API calls
+    config.enable_processing = true
+    config.cache_storage = :file
   end
 end
