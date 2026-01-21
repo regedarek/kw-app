c2724082e7f2a47c1d32bb2d4812603ad88fbe70
Darek Finster <dariusz.finster@gmail.com>
2026-01-21 21:33:00 +0100

    Fix: Restore missing business routes (courses, sign_ups, payments, lists)
    Business routes were accidentally removed in rails7-kamal2-cleanup commit.
    This restores routes for courses_path and related business functionality.

--- config/routes/custom_endpoints.rb
+++ config/routes/custom_endpoints.rb
@@ -1,3 +1,47 @@
+namespace :business do
+  resources :course_types, only: [:index]
+  resources :conversations, only: [:create] do
+    resources :messages, only: [:create]
+  end
+  resources :payments, only: [] do
+    member do
+      post :charge
+    end
+  end
+  resources :sign_ups do
+    member do
+      resources :lists, only: [:new, :create] do
+        collection do
+          post :ask
+        end
+      end
+      post :send_second
+    end
+  end
+  resources :lists, only: [:index]
+end
+
+scope module: 'business' do
+  resources :courses do
+    collection do
+      get :history
+    end
+    member do
+      get :public
+      put :seats_minus
+      put :seats_plus
+    end
+  end
+
+  namespace :api do
+    resources :courses, only: :index
+  end
+
+  get 'zapotrzebowanie/:id' => 'lists#new', as: 'business_list'
+  get 'kursy/:id' => 'courses#public', as: 'business_course_record_public'
+  get 'courses/:id' => 'courses#show', as: 'business_course_record'
+end
+
 namespace :olx do
   resources :sale_announcements, path: "ogloszenia"
 end
