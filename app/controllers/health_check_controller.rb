6f063835a3a8a860ff86e5003b8794a9669a6c94
Darek Finster <dariusz.finster@gmail.com>
2026-01-22 18:38:56 +0100

    Update health_check_controller.rb

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,9 +1,6 @@
 class HealthCheckController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!, if: :devise_controller?
   
-  rescue_from(Exception) { render plain: 'Service Unavailable', status: :service_unavailable }
-
   def show
     render plain: 'OK', status: :ok
   end
